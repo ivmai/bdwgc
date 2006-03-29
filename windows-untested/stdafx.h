cbc1a13321d9e24cb028e5871cb978ac7502476c
hboehm <hboehm>
2006-03-29 01:22:16 +0000

    2006-03-28  Hans Boehm <Hans.Boehm@hp.com> (Really Andrei Polushin) 	Initial checkin of VC++ Project and support files. 	* Added: 	* windows-untested/gc.def windows-untested/gc.rc 	* windows-untested/gc.ver windows-untested/stdafx.c 	* windows-untested/stdafx.h windows-untested/vc60/all.dsp 	* windows-untested/vc60/gc.dsp windows-untested/vc60/gc.dsw 	* windows-untested/vc60/libgc.dsp 	* windows-untested/vc60/libgcmt.dsp 	* windows-untested/vc60/test.dsp 	* windows-untested/vc60/test_gc.dsp 	* windows-untested/vc60/test_leak_gc.dsp 	* windows-untested/vc60/test_leak_libgc.dsp 	* windows-untested/vc60/test_leak_libgcmt.dsp 	* windows-untested/vc60/test_libgc.dsp 	* windows-untested/vc60/test_libgcmt.dsp 	* windows-untested/vc60/vc60crlf.cmd 	* windows-untested/vc70/all.vcproj windows-untested/vc70/gc.sln 	* windows-untested/vc70/gc.vcproj 	* windows-untested/vc70/libgc.vcproj 	* windows-untested/vc70/libgcmt.vcproj 	* windows-untested/vc70/test.vcproj 	* windows-untested/vc70/test_gc.vcproj 	* windows-untested/vc70/test_leak_gc.vcproj 	* windows-untested/vc70/test_leak_libgc.vcproj 	* windows-untested/vc70/test_leak_libgcmt.vcproj 	* windows-untested/vc70/test_libgc.vcproj 	* windows-untested/vc70/test_libgcmt.vcproj 	* windows-untested/vc71/all.vcproj windows-untested/vc71/gc.sln 	* windows-untested/vc71/gc.vcproj 	* windows-untested/vc71/libgc.vcproj 	* windows-untested/vc71/libgcmt.vcproj 	* windows-untested/vc71/test.vcproj 	* windows-untested/vc71/test_gc.vcproj 	* windows-untested/vc71/test_leak_gc.vcproj 	* windows-untested/vc71/test_leak_libgc.vcproj 	* windows-untested/vc71/test_leak_libgcmt.vcproj 	* windows-untested/vc71/test_libgc.vcproj 	* windows-untested/vc71/test_libgcmt.vcproj

---
+++ windows-untested/stdafx.h
@@ -0,0 +1,20 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#pragma warning(error: 4013) // function undefined; assuming extern returning int
+
+#ifdef _MT
+#  define GC_THREADS 1
+#endif
+
+#ifdef _DEBUG
+#  define GC_DEBUG
+#endif
+
+#define SILENT 1
+
+#define __STDC__ 1
+
+#define SAVE_CALL_CHAIN
+
+#define SAVE_CALL_COUNT 8
