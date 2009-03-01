e47c408d4433661a2d7586479f9bd12fbf1687c8
hboehm <hboehm>
2009-03-01 01:17:36 +0000

    2009-02-28  Hans Boehm <Hans.Boehm@hp.com> (Really Petr Krajca) 	* configure.ac: Enable thread-local allocation for sparc-linux. 	* configure: Regenerate.

--- misc.c
+++ misc.c
@@ -146,7 +146,7 @@ STATIC void GC_init_size_map(void)
         GC_size_map[i] = ROUNDED_UP_GRANULES(i);
 #       ifndef _MSC_VER
           GC_ASSERT(GC_size_map[i] < TINY_FREELISTS);
-	  /* Seems to tickle bug in some versions of VC++ */
+	  /* Seems to tickle bug in VC++ 2008 for AMD64 */
 #	endif
     }
     /* We leave the rest of the array to be filled in on demand. */
