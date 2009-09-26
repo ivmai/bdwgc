a7b3be020d2ba047f983eb24baaa636b388b1b6e
ivmai <ivmai>
2009-09-26 10:51:59 +0000

    2009-09-26  Ivan Maidanski <ivmai@mail.ru> (really Petter Urkedal)
    	* autogen.sh: Update.

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ set -e
 # These version are ok, pre-1.7 is not.  Post 1.7 may produce a lot of
 # warnings for unrelated projects, so prefer 1.7 for now.
 am_version=
-for v in 1.7 1.9 1.8; do
+for v in 1.10 1.9 1.8 1.7; do
     if type -p &>/dev/null automake-$v; then
 	am_version="-$v"
 	break
@@ -21,10 +21,11 @@ if [ -z "$am_version" ]; then
 fi
 
 set -x
-libtoolize --automake --force
 aclocal$am_version
-automake$am_version -ac
 autoconf
+autoheader
+automake$am_version -ac
+libtoolize --automake --force
 set +x
 echo
 echo "Ready to run './configure'."
