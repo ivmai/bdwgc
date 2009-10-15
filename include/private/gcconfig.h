e52b90d745b75049d773acd47da1ffd33251195b
ivmai <ivmai>
2009-10-15 16:41:07 +0000

    2009-10-15  Ivan Maidanski <ivmai@mail.ru>
    	* include/private/gcconfig.h: Define DebugBreak() as _exit(-1) for
    	x86mingw32ce toolchain to workaround the incorrect DebugBreak()
    	declaration in winbase.h (the workaround would turn into a no-op
    	when DebugBreak() will be defined as a macro in the toolchain).

--- include/private/gcconfig.h
+++ include/private/gcconfig.h
@@ -374,6 +374,10 @@
 #   endif
 #   if defined(x86) || defined(__i386__)
 #     define I386
+#     if defined(__MINGW32CE__) && !defined(DebugBreak)
+        /* x86mingw32ce toolchain doesn't have proper DebugBreak() now. */
+#       define DebugBreak() _exit(-1)
+#     endif
 #   endif
 #   if defined(_M_ARM) || defined(ARM) || defined(_ARM_)
 #     define ARM32
