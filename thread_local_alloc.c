d604870504cc8dd8870e8c1a6e719f88eb6c5ba7
ivmai <ivmai>
2011-05-10 15:39:29 +0000

    2011-05-10  Ivan Maidanski  <ivmai@mail.ru>
    	* thread_local_alloc.c (GC_malloc, GC_malloc_atomic): Join
    	adjacent "#ifdef".
    	* thread_local_alloc.c (GC_malloc_atomic): Call
    	GC_core_malloc_atomic (instead of GC_core_malloc).

--- thread_local_alloc.c
+++ thread_local_alloc.c
@@ -147,8 +147,6 @@ GC_API void * GC_CALL GC_malloc(size_t bytes)
       tsd = GC_getspecific(k);
 #   else
       tsd = GC_getspecific(GC_thread_key);
-#   endif
-#   if defined(USE_PTHREAD_SPECIFIC) || defined(USE_WIN32_SPECIFIC)
       if (EXPECT(0 == tsd, FALSE)) {
         return GC_core_malloc(bytes);
       }
@@ -179,15 +177,13 @@ GC_API void * GC_CALL GC_malloc_atomic(size_t bytes)
       if (EXPECT(0 == k, FALSE)) {
         /* We haven't yet run GC_init_parallel.  That means     */
         /* we also aren't locking, so this is fairly cheap.     */
-        return GC_core_malloc(bytes);
+        return GC_core_malloc_atomic(bytes);
       }
       tsd = GC_getspecific(k);
 #   else
       tsd = GC_getspecific(GC_thread_key);
-#   endif
-#   if defined(USE_PTHREAD_SPECIFIC) || defined(USE_WIN32_SPECIFIC)
       if (EXPECT(0 == tsd, FALSE)) {
-        return GC_core_malloc(bytes);
+        return GC_core_malloc_atomic(bytes);
       }
 #   endif
     GC_ASSERT(GC_is_initialized);
