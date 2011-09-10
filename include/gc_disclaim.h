8f24e4597184abcbe5ad3a274d5edded213e044e
Petter Urkedal <paurkedal@gmail.com>
2011-09-10 15:09:41 +0200

    Add and update some copyright and remove README.disclaim.
    * README.disclaim: Remove as it only contained patching instructions.
    * disclaim.c: Add copyright notice.
    * include/gc_disclaim.h: Update year on copyright notice.

--- include/gc_disclaim.h
+++ include/gc_disclaim.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 by Hewlett-Packard Company. All rights reserved.
+ * Copyright (c) 2007-2011 by Hewlett-Packard Company. All rights reserved.
  *
  * THIS MATERIAL IS PROVIDED AS IS, WITH ABSOLUTELY NO WARRANTY EXPRESSED
  * OR IMPLIED.  ANY USE IS AT YOUR OWN RISK.
