1b084eb5412aa0a6f7b8f7767841b810f1b17e8d
David Daney <ddaney@avtrex.com>
2011-12-27 11:18:11 +0400

    Fix gcconfig.h to handle mips64-linux target (Apply part of commit 3cf0bae from 'gcc_boehmgc' branch)
    * include/private/gcconfig.h (CPP_WORDSZ, ALIGNMENT): Handle
    mips64-linux n64 ABI.

--- include/private/gcconfig.h
+++ include/private/gcconfig.h
@@ -1463,8 +1463,12 @@
 #     define DATAEND (ptr_t)(_end)
       extern int __data_start[];
 #     define DATASTART ((ptr_t)(__data_start))
-#     define CPP_WORDSZ _MIPS_SZPTR
-#     define ALIGNMENT (_MIPS_SZPTR/8)
+#     ifdef _MIPS_SZPTR
+#       define CPP_WORDSZ _MIPS_SZPTR
+#       define ALIGNMENT (_MIPS_SZPTR/8)
+#     else
+#       define ALIGNMENT 4
+#     endif
 #     if __GLIBC__ == 2 && __GLIBC_MINOR__ >= 2 || __GLIBC__ > 2
 #       define LINUX_STACKBOTTOM
 #     else
