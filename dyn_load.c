7efb745562da0fc7e50380247a6a3cbe962b62a6
Ivan Maidanski <ivmai@mail.ru>
2012-01-25 19:18:39 +0400

    Remove Android-specific code inside Solaris-specific code in dyn_load
    * dyn_load.c (GC_register_dynamic_libraries): Remove PLATFORM_ANDROID
    macro check from Solaris-specific code.

--- dyn_load.c
+++ dyn_load.c
@@ -174,6 +174,7 @@ GC_FirstDLOpenedLinkMap(void)
 # endif
 
 # if defined(SOLARISDL)
+
 /* Add dynamic library data sections to the root set.           */
 # if !defined(PCR) && !defined(GC_SOLARIS_THREADS) && defined(THREADS)
         --> fix mutual exclusion with dlopen
@@ -192,10 +193,6 @@ GC_INNER void GC_register_dynamic_libraries(void)
         int i;
 
         e = (ElfW(Ehdr) *) lm->l_addr;
-#       ifdef PLATFORM_ANDROID
-          if (e == NULL)
-            continue;
-#       endif
         p = ((ElfW(Phdr) *)(((char *)(e)) + e->e_phoff));
         offset = ((unsigned long)(lm->l_addr));
         for( i = 0; i < (int)e->e_phnum; i++, p++ ) {
