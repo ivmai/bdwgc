5a9b1a5cb59fc44e4f11fe26148ad61a5ac6f9ea
Ivan Maidanski <ivmai@mail.ru>
2012-02-20 08:44:03 +0400

    Simplify autogen.sh by using autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,32 +1,9 @@
-#! /bin/sh
+#!/bin/sh
 
-set -e
+# This script creates (or regenerates) configure (as well as aclocal.m4,
+# config.h.in, Makefile.in, etc.) missing in the source repository.
 
-# These version are ok, pre-1.7 is not.  Post 1.7 may produce a lot of
-# warnings for unrelated projects, so prefer 1.7 for now.
-am_version=
-for v in 1.11 1.10 1.9 1.8 1.7; do
-    if type -p &>/dev/null automake-$v; then
-	am_version="-$v"
-	break
-    fi
-done
-if [ -z "$am_version" ]; then
-    case "`automake --version`" in
-	*\ 0.*|*\ 1.[0-6].*|*\ 1.[0-6]\ *)
-	    echo "$0: Automake-1.7 or later is needed."
-	    exit 2
-	    ;;
-    esac
-fi
+autoreconf -i
 
-set -x
-aclocal$am_version
-autoconf
-autoheader
-libtoolize --automake --force
-automake$am_version -ac
-set +x
 echo
 echo "Ready to run './configure'."
-echo
