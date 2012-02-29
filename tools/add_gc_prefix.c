26aec11628a60a5ac92ba7a55e729b5d0138c85c
Ivan Maidanski <ivmai@mail.ru>
2012-02-29 08:39:50 +0400

    tools: Change some functions declaration style from K-R to ANSI C
    * tools/add_gc_prefix.c (main): Remove unused "envp" argument.
    * tools/add_gc_prefix.c (main): Replace the K&R-style function
    definition with the ANSI C one.
    * tools/gcname.c (main): Likewise.

--- tools/add_gc_prefix.c
+++ tools/add_gc_prefix.c
@@ -1,19 +1,16 @@
 # include <stdio.h>
 # include <gc.h>
 
-int main(argc, argv, envp)
-int argc;
-char ** argv;
-char ** envp;
+int main(int argc, char ** argv)
 {
     int i;
 
     for (i = 1; i < argc; i++) {
       if (GC_ALPHA_VERSION == GC_NOT_ALPHA) {
-	printf("gc%d.%d/%s ", GC_VERSION_MAJOR, GC_VERSION_MINOR, argv[i]);
+        printf("gc%d.%d/%s ", GC_VERSION_MAJOR, GC_VERSION_MINOR, argv[i]);
       } else {
-	printf("gc%d.%dalpha%d/%s ", GC_VERSION_MAJOR,
-	       GC_VERSION_MINOR, GC_ALPHA_VERSION, argv[i]);
+        printf("gc%d.%dalpha%d/%s ", GC_VERSION_MAJOR,
+               GC_VERSION_MINOR, GC_ALPHA_VERSION, argv[i]);
       }
     }
     return(0);
