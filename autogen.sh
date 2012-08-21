7fdcd363e14919a211035bd99b34baa8662d2377
Petter Urkedal <paurkedal@gmail.com>
2012-08-21 23:08:08 +0200

    Elaborate comment on dependencies in autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,14 @@ set -e
 
 # This script creates (or regenerates) configure (as well as aclocal.m4,
 # config.h.in, Makefile.in, etc.) missing in the source repository.
-
-# Note: in case of problems (e.g., in cygwin/mingw or OS X), please make
-# sure 'pkg-config' is installed on your host.
+#
+# If you compile from a distribution tarball, you can skip this.  Otherwise,
+# make sure that you have Autoconf, Automake, Libtool, and pkg-config
+# installed on your system, and that the corresponding *.m4 files are visible
+# to the aclocal.  The latter can be achieved by using packages shipped by
+# your OS, or by installing custom versions of all four packages to the same
+# prefix.  Otherwise, you may need to invoke autoreconf with the appropriate
+# -I options to locate the required *.m4 files.
 
 autoreconf -i
 
