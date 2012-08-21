c6da60e9c5907244e6d28dde06f4c76ac6ac127d
Ivan Maidanski <ivmai@mail.ru>
2012-08-21 22:09:50 +0400

    Add note about 'pkg-config' solving problem with autoconf 2.68 or older (Cygwin/MinGW, OS X)
    * autogen.sh: Add comment about 'pkg-config'.
    * TODO: Remove note about 'autoreconf' failure.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,9 @@ set -e
 # This script creates (or regenerates) configure (as well as aclocal.m4,
 # config.h.in, Makefile.in, etc.) missing in the source repository.
 
+# Note: in case of problems (e.g., in cygwin/mingw or OS X), please make
+# sure 'pkg-config' is installed on your host.
+
 autoreconf -i
 
 echo
