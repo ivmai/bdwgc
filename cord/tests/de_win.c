58cce9853c9eee3c5390977c0949414874eba4c5
Ivan Maidanski <ivmai@mail.ru>
2014-06-15 11:39:12 +0400

    Fix signedness of char values passed to isspace, iscntrl, isxdigit (eliminate 'array subscript has type char' GCC warning)
    * cord/tests/de_win.c (WinMain, plain_chars, control_chars): Cast char
    value to unsigned char when passed to isspace(), iscntrl().
    * os_dep.c (GC_parse_map_entry): Change type of "start_start",
    "end_start", "maj_dev_start", "p" local variables from char* to
    unsgined char* (as isspace(), isxdigit() expect non-negative values);
    insert casts between char* and unsigned char* where needed.
    * os_dep.c (GC_parse_map_entry): Remove "endp" local variable (use "p"
    variable directly instead).

--- cord/tests/de_win.c
+++ cord/tests/de_win.c
@@ -89,7 +89,8 @@ int APIENTRY WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
    } else {
         char *p = command_line;
 
-        while (*p != 0 && !isspace(*p)) p++;
+        while (*p != 0 && !isspace(*(unsigned char *)p))
+            p++;
         arg_file_name = CORD_to_char_star(
                             CORD_substr(command_line, 0, p - command_line));
    }
@@ -133,7 +134,7 @@ char * plain_chars(char * text, size_t len)
     register size_t i;
 
     for (i = 0; i < len; i++) {
-       if (iscntrl(text[i])) {
+       if (iscntrl(((unsigned char *)text)[i])) {
            result[i] = ' ';
        } else {
            result[i] = text[i];
@@ -151,7 +152,7 @@ char * control_chars(char * text, size_t len)
     register size_t i;
 
     for (i = 0; i < len; i++) {
-       if (iscntrl(text[i])) {
+       if (iscntrl(((unsigned char *)text)[i])) {
            result[i] = text[i] + 0x40;
        } else {
            result[i] = ' ';
