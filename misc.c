c3ecfd89d30484ede9c88e6744f93aee5368e1bf
Ivan Maidanski <ivmai@mail.ru>
2014-06-19 03:04:28 +0400

    Fix unresolved vsnprintf in misc.c and snprintf in cordtest (DJGPP, VC)
    * cord/tests/cordtest.c (GC_SNPRINTF, GC_SNPRINTF_BUFSZ_ARG): New
    macro to workaround snprintf() missing in DJGPP and MS VC.
    * cord/tests/cordtest.c (test_printf): Replace snprintf() with
    GC_SNPRINTF and GC_SNPRINTF_BUFSZ_ARG.
    * misc.c (GC_VSNPRINTF): Test DJGPP instead of NO_VSNPRINTF; refine
    comment.

--- misc.c
+++ misc.c
@@ -1497,8 +1497,8 @@ GC_API void GC_CALL GC_enable_incremental(void)
 
 #define BUFSZ 1024
 
-#ifdef NO_VSNPRINTF
-  /* In case this function is missing (e.g., in DJGPP v2.0.3).  */
+#ifdef DJGPP
+  /* vsnprintf is missing in DJGPP (v2.0.3) */
 # define GC_VSNPRINTF(buf, bufsz, format, args) vsprintf(buf, format, args)
 #elif defined(_MSC_VER)
 # ifdef MSWINCE
