c58cbbaa1bf3015a6e092428289d54c43a275658
meister <meister@meisters-MacBook-Pro.local>
2014-10-16 09:49:00 -0400

    added heap walking code to reclaim.c

--- reclaim.c
+++ reclaim.c
@@ -772,3 +772,60 @@ GC_INNER GC_bool GC_reclaim_all(GC_stop_func stop_func, GC_bool ignore_old)
     }
   }
 #endif /* !EAGER_SWEEP && ENABLE_DISCLAIM */
+
+
+
+
+/*
+ * Code to walk the entire heap of live objects
+ *
+ */
+extern __attribute__((weak)) void GC_callback_reachable_object( GC_word* ptr, size_t sz)
+{
+    printf("Reachable object@%p sz[%lu]\n", ptr, sz);
+}
+
+
+/* Added by Christian Schafmeister June 2014
+   From code provided by Peter Wang in the bdwgc-request@lists.opendylan.com 
+   mailing list.
+   From: Peter Wang novalazy@gmail.com
+   To: Christian Schafmeister chris.schaf@verizon.net
+   Cc: bdwgc@lists.opendylan.org
+   Subject: Re: [Gc] Is there a way to walk the entire heap of live objects
+   Message-ID: 20140606141627.GC25889@lucy.localdomain
+*/
+STATIC void GC_mercury_do_enumerate_reachable_objects(struct hblk *hbp, word dummy)
+{
+    struct hblkhdr * hhdr = HDR(hbp);
+    size_t sz = hhdr -> hb_sz;
+    size_t bit_no;
+    char *p, *plim;
+
+    if (GC_block_empty(hhdr)) {
+        return;
+    }
+
+    p = hbp->hb_body;
+    bit_no = 0;
+    if (sz > MAXOBJBYTES) { /* one big object */
+        plim = p;
+    } else {
+        plim = hbp->hb_body + HBLKSIZE - sz;
+    }
+    /* Go through all words in block. */
+    while (p <= plim) {
+        if (mark_bit_from_hdr(hhdr, bit_no)) {
+            GC_callback_reachable_object((GC_word *)p,
+                                                 BYTES_TO_WORDS(sz));
+        }
+        bit_no += MARK_BIT_OFFSET(sz);
+        p += sz;
+    }
+}
+
+GC_INNER void GC_mercury_enumerate_reachable_objects(void)
+{
+    GC_ASSERT(GC_callback_reachable_object);
+    GC_apply_to_all_blocks(GC_mercury_do_enumerate_reachable_objects, (word)0);
+}
