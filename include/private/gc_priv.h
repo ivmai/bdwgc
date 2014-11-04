98b634d05ae474c6fe8913b472a0aacfc8304677
Ivan Maidanski <ivmai@mail.ru>
2014-11-04 17:01:08 +0300

    Fix mistyped ARM_THREAD_STATE macro (Darwin/arm) (Apply commit 1bfd1cd from 'release-7_4' branch.)
    * include/private/gc_priv.h (GC_MACH_THREAD_STATE,
    GC_MACH_THREAD_STATE_COUNT): Define to ARM_THREAD_STATE[_COUNT],
    respectively, instead of mistyped ARM_MACHINE_THREAD_STATE[_COUNT]
    (if DARWIN and ARM32).
    
    Conflicts:
    
        include/private/gc_priv.h

--- include/private/gc_priv.h
+++ include/private/gc_priv.h
@@ -513,12 +513,14 @@ GC_EXTERN GC_warn_proc GC_current_warn_proc;
 #     define GC_MACH_THREAD_STATE       x86_THREAD_STATE64
 #     define GC_MACH_THREAD_STATE_COUNT x86_THREAD_STATE64_COUNT
 #   endif
+# elif defined(ARM32)
+#   define GC_THREAD_STATE_T            arm_thread_state_t
+#   define GC_MACH_THREAD_STATE         ARM_THREAD_STATE
+#   define GC_MACH_THREAD_STATE_COUNT   ARM_THREAD_STATE_COUNT
 # else
-#   if defined(ARM32)
-#     define GC_THREAD_STATE_T          arm_thread_state_t
-#   else
-#     error define GC_THREAD_STATE_T
-#   endif
+#   error define GC_THREAD_STATE_T
+# endif
+# ifndef GC_MACH_THREAD_STATE
 #   define GC_MACH_THREAD_STATE         MACHINE_THREAD_STATE
 #   define GC_MACH_THREAD_STATE_COUNT   MACHINE_THREAD_STATE_COUNT
 # endif
