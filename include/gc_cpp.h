ivmai/bdwgc

commit 3d784edf71be4d0eb24db3b003a906b63be6aa7a

put all the cpp stuff in header otherwise vc9 will mix up new and delete operators (due to arbitrary ordering during linking)
Conflicts:

	gc_cpp.cc
	include/gc_cpp.h

include/gc_cpp.h
@@ -278,17 +278,48 @@ inline void* operator new( size_t size, GC_NS_QUALIFY(GCPlacement) gcp,
   *  There seems to be no way to redirect new in this environment without
   *  including this everywhere.
   */
-# if _MSC_VER > 1020
-    void *operator new[]( size_t size );
-    void operator delete[]( void* obj );
-# endif
+#if _MSC_VER > 1020
+inline void *operator new[]( size_t size ){
+    return GC_MALLOC_UNCOLLECTABLE( size );}
 
-  void* operator new( size_t size );
-  void operator delete( void* obj );
+inline void operator delete[]( void* obj ) {
+    GC_FREE( obj );}
 
-  // This new operator is used by VC++ in case of Debug builds !
-  void* operator new( size_t size, int /* nBlockUse */,
-                     const char * szFileName, int nLine );
+#endif
+
+
+// void* operator new( size_t size);
+// void operator delete(void* obj);
+
+    // MOVED HERE FROM gc_hpp.cc!
+    inline void* operator new(size_t size)
+    {
+        return GC_MALLOC_UNCOLLECTABLE(size);
+    }
+
+    inline void operator delete(void* obj)
+    {
+        GC_FREE(obj);
+    }
+
+
+ // This new operator is used by VC++ in case of Debug builds !
+ inline void* operator new(  size_t size,
+                      int ,//nBlockUse,
+                      const char * szFileName,
+                      int nLine )
+    {
+#ifndef GC_DEBUG
+            return GC_malloc_uncollectable( size );
+#else
+            return GC_debug_malloc_uncollectable(size, szFileName, nLine);
+#endif
+    }
+inline void* operator new[](size_t size, int nBlockUse, const char* szFileName, int nLine)
+{
+    return operator new(size, nBlockUse, szFileName, nLine);
+}
+
 #endif /* _MSC_VER */
 
 #ifdef GC_OPERATOR_NEW_ARRAY

gc_cpp.cc
@@ -43,46 +43,44 @@ built-in "new" and "delete".
 # define GC_DECL_DELETE_THROW /* empty */
 #endif /* !GC_NEW_DELETE_NEED_THROW */
 
-void* operator new( size_t size ) GC_DECL_NEW_THROW {
-  return GC_MALLOC_UNCOLLECTABLE(size);
-}
-
-#if !defined(__CYGWIN__)
-  void operator delete( void* obj ) GC_DECL_DELETE_THROW {
-    GC_FREE(obj);
-  }
-#endif /* !__CYGWIN__ */
-
-#ifdef GC_OPERATOR_NEW_ARRAY
-  void* operator new[]( size_t size ) GC_DECL_NEW_THROW {
-    return GC_MALLOC_UNCOLLECTABLE(size);
-  }
-
-  void operator delete[]( void* obj ) GC_DECL_DELETE_THROW {
-    GC_FREE(obj);
-  }
-#endif /* GC_OPERATOR_NEW_ARRAY */
-
-#ifdef _MSC_VER
-
-  // This new operator is used by VC++ in case of Debug builds!
-  void* operator new( size_t size, int /* nBlockUse */,
-                     const char * szFileName, int nLine ) GC_DECL_NEW_THROW
-  {
-#   ifndef GC_DEBUG
-      return GC_malloc_uncollectable(size);
-#   else
-      return GC_debug_malloc_uncollectable(size, szFileName, nLine);
-#   endif
-  }
-
-# if _MSC_VER > 1020
-    // This new operator is used by VC++ 7.0 and later in Debug builds.
-    void* operator new[]( size_t size, int nBlockUse,
-                         const char* szFileName, int nLine ) GC_DECL_NEW_THROW
-    {
-      return operator new(size, nBlockUse, szFileName, nLine);
-    }
-# endif
-
-#endif /* _MSC_VER */
+// MOVED TO HEADER!
+//void* operator new( size_t size ) {
+//    return GC_MALLOC_UNCOLLECTABLE( size );}
+//
+//void operator delete( void* obj ) {
+//    GC_FREE( obj );}
+//
+//#ifdef GC_OPERATOR_NEW_ARRAY
+//
+//void* operator new[]( size_t size ) {
+//    return GC_MALLOC_UNCOLLECTABLE( size );}
+//
+//void operator delete[]( void* obj ) {
+//    GC_FREE( obj );}
+//
+//#endif /* GC_OPERATOR_NEW_ARRAY */
+//
+//#ifdef _MSC_VER
+//
+//// This new operator is used by VC++ in case of Debug builds !
+//void* operator new( size_t size,
+//                          int ,//nBlockUse,
+//                          const char * szFileName,
+//                          int nLine )
+//{
+//#ifndef GC_DEBUG
+//        return GC_malloc_uncollectable( size );
+//#else
+//        return GC_debug_malloc_uncollectable(size, szFileName, nLine);
+//#endif
+//}
+//
+//#if _MSC_VER > 1020
+//// This new operator is used by VC++ 7.0 and later in Debug builds.
+//void* operator new[](size_t size, int nBlockUse, const char* szFileName, int nLine)
+//{
+//    return operator new(size, nBlockUse, szFileName, nLine);
+//}
+//#endif
+//
+//#endif /* _MSC_VER */

commit 590b39d5b77cae520b32fac093ff6d0c22b393d4

Avoid new[] debug variant definition for ancient MS VC++ (restore behavior broken in commit 3d784ed)
* include/gc_cpp.h (operator new[]): Update comment.
* include/gc_cpp.h (operator new[]): Do not define the debug variant
for MS VC pre-v7; add comment.

Conflicts:

    include/gc_cpp.h

include/gc_cpp.h
@@ -281,6 +281,9 @@ inline void* operator new(size_t size, GC_NS_QUALIFY(GCPlacement) gcp,
   // if we define a multi-argument operator new[].
   // There seems to be no way to redirect new in this environment without
   // including this everywhere.
+  // Inlining done to avoid mix up of new and delete operators by VC++ 9 (due
+  // to arbitrary ordering during linking).
+
 # if _MSC_VER > 1020
     inline void* operator new[](size_t size)
     {
@@ -314,11 +317,14 @@ inline void* operator new(size_t size, GC_NS_QUALIFY(GCPlacement) gcp,
 #   endif
   }
 
-  inline void* operator new[](size_t size, int nBlockUse,
-                              const char* szFileName, int nLine)
-  {
-    return operator new(size, nBlockUse, szFileName, nLine);
-  }
+# if _MSC_VER > 1020
+    // This new operator is used by VC++ 7+ in Debug builds:
+    inline void* operator new[](size_t size, int nBlockUse,
+                                const char* szFileName, int nLine)
+    {
+      return operator new(size, nBlockUse, szFileName, nLine);
+    }
+# endif
 
 #endif // _MSC_VER
 