70fbfadd3842c828ae9dd10ef3212a2d0957ca86
Ivan Maidanski <ivmai@mail.ru>
2016-02-17 12:09:12 +0300

    Prevent code analysis tool warning about GC_base unchecked result
    * mark.c (GC_mark_and_push_stack): Add assertion on GC_base result is
    non-null (before use in HDR).

--- mark.c
+++ mark.c
@@ -1417,6 +1417,7 @@ GC_API struct GC_ms_entry * GC_CALL GC_mark_and_push(void *obj,
     if (EXPECT(IS_FORWARDING_ADDR_OR_NIL(hhdr), FALSE)) {
         if (hhdr != 0) {
           r = GC_base(p);
+          GC_ASSERT(r != NULL); /* to prevent a warning */
           hhdr = HDR(r);
         }
         if (hhdr == 0) {
