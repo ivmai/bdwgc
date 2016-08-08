6f4e123e34c2e20343a84b6fadd17bde0e7b354c
Ivan Maidanski <ivmai@mail.ru>
2016-08-08 21:49:20 +0300

    Fix 'unknown type name GC_INNER' compilation error (FreeBSD) (fix commit f13a9559)
    See issue #132.  The reason of compilation failure is GC_INNER macro
    used before its definition.  The solution is to move
    GC_FreeBSDGetDataStart prototype from gcconfig.h to gc_priv.h.
    
    * include/private/gc_priv.h [DATASTART_USES_BSDGETDATASTART]
    (GC_FreeBSDGetDataStart): Declare.
    * include/private/gc_priv.h [DATASTART_USES_BSDGETDATASTART]
    (DATASTART_IS_FUNC): Define macro.
    * include/private/gcconfig.h [DATASTART_USES_BSDGETDATASTART]
    (GC_FreeBSDGetDataStart, DATASTART_IS_FUNC): Remove.

--- include/private/gc_priv.h
+++ include/private/gc_priv.h
@@ -2500,6 +2500,8 @@ GC_INNER ptr_t GC_store_debug_info(ptr_t p, word sz, const char *str,
 # if !defined(PCR)
 #   define NEED_FIND_LIMIT
 # endif
+  GC_INNER ptr_t GC_FreeBSDGetDataStart(size_t, ptr_t);
+# define DATASTART_IS_FUNC
 #endif /* DATASTART_USES_BSDGETDATASTART */
 
 #if (defined(NETBSD) || defined(OPENBSD)) && defined(__ELF__) \
