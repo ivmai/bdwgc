d0761f0d9c81c3d29009e75ac15623748268228d
Ivan Maidanski <ivmai@mail.ru>
2016-10-26 11:13:29 +0300

    Eliminate 'suspicious pointer subtraction' cppcheck warning (gc_cpp)
    * include/gc_cpp.h (gc_cleanup::gc_cleanup): Define this_ptr void
    pointer; use this_ptr instead of this keyword to outline that the
    pointer subtraction is intentional (i.e., "this->" was not intended).

--- include/gc_cpp.h
+++ include/gc_cpp.h
@@ -428,11 +428,12 @@ inline gc_cleanup::gc_cleanup()
 {
   GC_finalization_proc oldProc;
   void* oldData;
-  void* base = GC_base((void*) this);
+  void* this_ptr = (void*)this;
+  void* base = GC_base(this_ptr);
   if (base != 0) {
     // Don't call the debug version, since this is a real base address.
     GC_register_finalizer_ignore_self(base, (GC_finalization_proc) cleanup,
-                                      (void*) ((char*) this - (char*) base),
+                                      (void*)((char*)this_ptr - (char*)base),
                                       &oldProc, &oldData);
     if (oldProc != 0) {
       GC_register_finalizer_ignore_self(base, oldProc, oldData, 0, 0);
