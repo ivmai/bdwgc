6abf16fe24c2e57de2acf729fad03a4a6bc6e3a5
Ivan Maidanski <ivmai@mail.ru>
2017-09-27 11:42:46 +0300

    Eliminate '-pedantic is not option that controls warnings' GCC-6.4 message (Cherry-pick commit e0a3739 from 'master' branch.)
    * mark.c [WRAP_MARK_SOME && (MSWIN32 || MSWINCE) && __GNUC__]
    (GC_mark_some): Use "-Wpedantic" instead of "-pedantic" (in pragma
    GCC diagnostic) if GCC v6.4+.

--- mark.c
+++ mark.c
@@ -522,7 +522,8 @@ static void alloc_mark_stack(size_t);
          || (__clang_major__ == 3 && __clang_minor__ >= 3)
 #       pragma GCC diagnostic push
         /* Suppress "taking the address of label is non-standard" warning. */
-#       if defined(__clang__) || __GNUC__ >= 7
+#       if defined(__clang__) || __GNUC__ > 6 \
+           || (__GNUC__ == 6 && __GNUC_MINOR__ >= 4)
 #         pragma GCC diagnostic ignored "-Wpedantic"
 #       else
           /* GCC before ~4.8 does not accept "-Wpedantic" quietly.  */
