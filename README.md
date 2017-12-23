72456d094bb68b0fe9e3c01bef453405f81806f8
Ivan Maidanski <ivmai@mail.ru>
2017-12-23 11:01:34 +0300

    [7.6.2]
    Bump gc version to 7.6.2
    
    * ChangeLog (7.6.2): Set release date.
    * Makefile.am (LIBGC_VER_INFO): Increment revision (change version
    info of libgc.so to 4:1:2).
    * README.md: Bump micro version.
    * configure.ac (AC_INIT): Likewise.
    * include/gc_version.h (GC_TMP_VERSION_MICRO): Likewise.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Boehm-Demers-Weiser Garbage Collector
 
-This is version 7.6.0 of a conservative garbage
+This is version 7.6.2 of a conservative garbage
 collector for C and C++.
 
 
--- include/gc_version.h
+++ include/gc_version.h
@@ -30,7 +30,7 @@
 /* it to keep the old-style build process working.              */
 #define GC_TMP_VERSION_MAJOR 7
 #define GC_TMP_VERSION_MINOR 6
-#define GC_TMP_VERSION_MICRO 0 /* 7.6.0 */
+#define GC_TMP_VERSION_MICRO 2 /* 7.6.2 */
 
 #ifdef GC_VERSION_MAJOR
 # if GC_TMP_VERSION_MAJOR != GC_VERSION_MAJOR \
