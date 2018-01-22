aa828d4e12b0664b10db15058e1da6d666630ae1
Ivan Maidanski <ivmai@mail.ru>
2018-01-22 13:31:59 +0300

    [7.4.10]
    Bump gc version to 7.4.10
    
    * Makefile.am (LIBGC_VER_INFO): Increment revision (change version
    info of libgc.so to 3:3:2).
    * README.md: Bump micro version.
    * configure.ac (AC_INIT): Likewise.
    * include/gc_version.h (GC_TMP_VERSION_MICRO): Likewise.

--- include/gc_version.h
+++ include/gc_version.h
@@ -30,7 +30,7 @@
 /* it to keep the old-style build process working.              */
 #define GC_TMP_VERSION_MAJOR 7
 #define GC_TMP_VERSION_MINOR 4
-#define GC_TMP_VERSION_MICRO 8 /* 7.4.8 */
+#define GC_TMP_VERSION_MICRO 10 /* 7.4.10 */
 
 #ifdef GC_VERSION_MAJOR
 # if GC_TMP_VERSION_MAJOR != GC_VERSION_MAJOR \
