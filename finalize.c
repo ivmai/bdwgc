40a8fd8c1d527b9c3b5d2c0cd5d2b1ea49319ac0
Ivan Maidanski <ivmai@mail.ru>
2018-07-04 17:33:52 +0300

    Eliminate duplicate clear_mark_bit call when removing disappearing link (fix of commit 8d13d52b6)
    * finalize.c (GC_make_disappearing_links_disappear,
    GC_remove_dangling_disappearing_links): Remove GC_clear_mark_bit call.

--- finalize.c
+++ finalize.c
@@ -946,7 +946,6 @@ GC_INLINE void GC_make_disappearing_links_disappear(
 
         if (!GC_is_marked(real_ptr)) {
             *(word *)real_link = 0;
-            GC_clear_mark_bit(curr);
             DELETE_DL_HASHTBL_ENTRY(dl_hashtbl, curr, prev, next);
         }
     ITERATE_DL_HASHTBL_END(curr, prev)
@@ -961,7 +960,6 @@ GC_INLINE void GC_remove_dangling_disappearing_links(
         ptr_t real_link = GC_base(GC_REVEAL_POINTER(curr -> dl_hidden_link));
 
         if (NULL != real_link && !GC_is_marked(real_link)) {
-            GC_clear_mark_bit(curr);
             DELETE_DL_HASHTBL_ENTRY(dl_hashtbl, curr, prev, next);
         }
     ITERATE_DL_HASHTBL_END(curr, prev)
