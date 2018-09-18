25e85ca4d5470b10eca8db883d48443405fdefce
Petter Urkedal <paurkedal@gmail.com>
2018-09-19 00:23:47 +0300

    Fix marking of finalizer closure object (a cherry-pick of commits 21312a008, ea9d6dd6a from 'master')
    * fnlz_mlc.c [ENABLE_DISCLAIM]: Include dbg_mlc.h.
    * fnlz_mlc.c [ENABLE_DISCLAIM] (GC_init_finalized_malloc): Call
    GC_register_displacement_inner(FINALIZER_CLOSURE_FLAG) and
    GC_register_displacement_inner(sizeof(oh)+FINALIZER_CLOSURE_FLAG); add
    comment.
    * fnlz_mlc.c [ENABLE_DISCLAIM] (GC_finalized_malloc): Call GC_dirty and
    REACHABLE_AFTER_DIRTY after storing fclos.

--- fnlz_mlc.c
+++ fnlz_mlc.c
@@ -18,6 +18,7 @@
 
 #include "gc_disclaim.h"
 #include "gc_inline.h" /* for GC_malloc_kind */
+#include "private/dbg_mlc.h" /* for oh type */
 
 STATIC int GC_finalized_kind = 0;
 
@@ -69,6 +70,11 @@ GC_API void GC_CALL GC_init_finalized_malloc(void)
     /* start of the user region.                                        */
     GC_register_displacement_inner(sizeof(word));
 
+    /* And, the pointer to the finalizer closure object itself is       */
+    /* displaced due to baking in this indicator.                       */
+    GC_register_displacement_inner(FINALIZER_CLOSURE_FLAG);
+    GC_register_displacement_inner(sizeof(oh) + FINALIZER_CLOSURE_FLAG);
+
     GC_finalized_kind = GC_new_kind_inner(GC_new_free_list_inner(),
                                           GC_DS_LENGTH, TRUE, TRUE);
     GC_register_disclaim_proc(GC_finalized_kind, GC_finalized_disclaim, TRUE);
@@ -93,6 +99,8 @@ GC_API GC_ATTR_MALLOC void * GC_CALL GC_finalized_malloc(size_t lb,
     if (EXPECT(NULL == op, FALSE))
         return NULL;
     *op = (word)fclos | FINALIZER_CLOSURE_FLAG;
+    GC_dirty(op);
+    REACHABLE_AFTER_DIRTY(fclos);
     return op + 1;
 }
 
