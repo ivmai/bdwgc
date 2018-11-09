e9c88faa482b9c5bc1562de36cd2fcb09d9a66c7
Ivan Maidanski <ivmai@mail.ru>
2018-11-09 21:25:29 +0300

    Adjust #error messages format (code refactoring)
    Remove double quotes, "-->" prefix and the trailing dot from the error
    messages.
    
    * include/private/gcconfig.h [hp9000s300 && !CPPCHECK]: Adjust #error
    message format.
    * include/private/gcconfig.h [ibm032 && !CPPCHECK]: Likewise.
    * include/private/gcconfig.h [!mach_type_known && !CPPCHECK]: Likewise.
    * include/private/gcconfig.h [HEXAGON && (!__GLIBC__ || __GLIBC__ < 2
    || !__ELF__ && !CPPCHECK || !LINUX)]: Likewise.
    * include/private/gcconfig.h [!ALIGNMENT && !CPPCHECK]: Likewise.
    * include/private/gcconfig.h [!STACKBOTTOM && (ECOS || NOSYS)
    && !CPPCHECK]: Likewise.
    * include/private/gcconfig.h [!CPPCHECK && (GC_IRIX_THREADS
    || GC_LINUX_THREADS || GC_NETBSD_THREADS || GC_FREEBSD_THREADS
    || GC_SOLARIS_THREADS || GC_HPUX_THREADS || GC_AIX_THREADS
    || GC_WIN32_THREADS || GC_WIN32_PTHREADS)]: Likewise.
    * include/private/gcconfig.h [PARALLEL_MARK && !THREADS && !CPPCHECK]:
    Likewise.
    * include/private/gcconfig.h [!CPPCHECK && (MARK_BIT_PER_GRANULE
    && MARK_BIT_PER_OBJ || STACK_GROWS_UP && STACK_GROWS_DOWN
    || !STACK_GROWS_UP && !STACK_GROWS_DOWN || REDIRECT_MALLOC && THREADS
    && !LINUX && !REDIRECT_MALLOC_IN_HEADER)]: Likewise.
    * os_dep.c [(MMAP_SUPPORTED || ADD_HEAP_GUARD_PAGES) && USE_MUNMAP
    && !USE_MMAP && !CPPCHECK]: Likewise.
    * win32_threads.c [!I386 && !X86_64 && !ARM32 && !AARCH64 && !SHx
    !MIPS && !PPC && !ALPHA && !CPPCHECK] (GC_push_stack_for): Likewise.

--- os_dep.c
+++ os_dep.c
@@ -71,7 +71,7 @@
 
 #if defined(MMAP_SUPPORTED) || defined(ADD_HEAP_GUARD_PAGES)
 # if defined(USE_MUNMAP) && !defined(USE_MMAP) && !defined(CPPCHECK)
-#   error "invalid config - USE_MUNMAP requires USE_MMAP"
+#   error Invalid config: USE_MUNMAP requires USE_MMAP
 # endif
 # include <sys/types.h>
 # include <sys/mman.h>
