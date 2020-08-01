e7c50427d3203d0237ee8cfcc3c484a59bfa5d13
Ivan Maidanski <ivmai@mail.ru>
2020-08-01 15:40:43 +0300

    Use compiler TLS on FreeBSD
    Issue #325 (bdwgc).
    
    Previously, compiler TLS (USE_COMPILER_TLS) was used on kFreeBSD but
    pthread_getspecific() was used instead on other FreeBSD flavors.
    
    * include/private/thread_local_alloc.h  [!USE_PTHREAD_SPECIFIC
    && !USE_WIN32_SPECIFIC && !USE_WIN32_COMPILER_TLS && !USE_COMPILER_TLS
    && !USE_CUSTOM_SPECIFIC && !(MSWIN32 || MSWINCE || CYGWIN32)
    && FREEBSD && GC_GNUC_PREREQ(3,3)] (USE_COMPILER_TLS): Define macro
    (regardless of __GLIBC__).

--- include/private/thread_local_alloc.h
+++ include/private/thread_local_alloc.h
@@ -55,7 +55,7 @@ EXTERN_C_BEGIN
 # elif (defined(LINUX) && !defined(ARM32) && !defined(AVR32) \
          && GC_GNUC_PREREQ(3, 3) \
          && !(defined(__clang__) && defined(HOST_ANDROID))) \
-       || (defined(FREEBSD) && defined(__GLIBC__) /* kFreeBSD */ \
+       || (defined(FREEBSD) \
             && GC_GNUC_PREREQ(4, 4)) \
        || (defined(HOST_ANDROID) && defined(ARM32) \
             && (GC_GNUC_PREREQ(4, 6) || GC_CLANG_PREREQ_FULL(3, 8, 256229)))
