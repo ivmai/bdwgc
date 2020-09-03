fbcdf44c4cbe1eadec9ee4e6ba2c389e72bca155
Ivan Maidanski <ivmai@mail.ru>
2020-09-03 19:04:26 +0300

    Fix copyright message in de_win.rc and gc_cpp.cc
    Issue #331 (bdwgc).
    
    The copyright message in these 2 files was different from the BDWGC
    default one (e.g. see that in malloc.c) by a mistake.
    
    * cord/tests/de_win.rc: Alter copyright to match that of de_win.c.
    * gc_cpp.cc: Alter copyright to match that of gc_cpp.h.

--- gc_cpp.cc
+++ gc_cpp.cc
@@ -4,8 +4,11 @@
  * THIS MATERIAL IS PROVIDED AS IS, WITH ABSOLUTELY NO WARRANTY EXPRESSED
  * OR IMPLIED.  ANY USE IS AT YOUR OWN RISK.
  *
- * Permission is hereby granted to copy this code for any purpose,
- * provided the above notices are retained on all copies.
+ * Permission is hereby granted to use or copy this program
+ * for any purpose,  provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
  */
 
 /*************************************************************************
