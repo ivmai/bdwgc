e089c26b96aa1612cbd8c4e5774b91073e34ea0a
Ivan Maidanski <ivmai@mail.ru>
2021-06-15 11:10:30 +0300

    Remove dead part of condition to define NEED_FIND_LIMIT in gc_priv.h (fix of commit 8a1f78a27)
    * include/private/gc_priv.h (NEED_FIND_LIMIT): Do not depend on
    whether STACKBOTTOM is defined.

--- include/private/gc_priv.h
+++ include/private/gc_priv.h
@@ -2651,7 +2651,6 @@ GC_INNER void *GC_store_debug_info_inner(void *p, word sz, const char *str,
 /* Do we need the GC_find_limit machinery to find the end of a  */
 /* data segment.                                                */
 #if defined(HEURISTIC2) || defined(SEARCH_FOR_DATA_START) \
-    || (!defined(STACKBOTTOM) && defined(HEURISTIC2)) \
     || ((defined(SVR4) || defined(AIX) || defined(DGUX) \
          || (defined(LINUX) && defined(SPARC))) && !defined(PCR))
 # define NEED_FIND_LIMIT
