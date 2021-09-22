ea07c62dca4c48a7f689ca7912d86894699fb138
Ivan Maidanski <ivmai@mail.ru>
2021-09-22 21:45:00 +0300

    Always print '#' symbol before GC number (refactoring)
    * backgraph.c [MAKE_BACK_GRAPH] (GC_print_back_graph_stats): Print '#' before
    GC number.

--- backgraph.c
+++ backgraph.c
@@ -535,8 +535,9 @@ GC_INNER void GC_traverse_back_graph(void)
 void GC_print_back_graph_stats(void)
 {
   GC_ASSERT(I_HOLD_LOCK());
-  GC_printf("Maximum backwards height of reachable objects at GC %lu is %lu\n",
-            (unsigned long) GC_gc_no, (unsigned long)GC_max_height);
+  GC_printf("Maximum backwards height of reachable objects"
+            " at GC #%lu is %lu\n",
+            (unsigned long)GC_gc_no, (unsigned long)GC_max_height);
   if (GC_max_height > GC_max_max_height) {
     ptr_t obj = GC_deepest_obj;
 
