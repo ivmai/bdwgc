fae478c27c7a1b6e20c80a9da6dcfd948f2861ae
Dejice Jacob <dejice.jacob@glasgow.ac.uk>
2021-11-24 16:09:49 +0000

    add unit-tests infrastructure for boehmGC on riscv64-purecap
    The script initializes and starts a riscv64-purecap qemu-VM instance,
    compiles test code, transfers it to the qemu-VM  and executes it.

--- .buildbot.sh
+++ .buildbot.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-# arg-1 : cmake toolchain file
+echo "building for riscv64-purecap"
+CHERIBUILD=~/build
+PLATFORMS='riscv64-purecap'
+
+export SSHPORT=10021
+export SSHHOST=localhost
+export PYTHONPATH="$CHERIBUILD"/test-scripts
+
+# arg-1 : platform to build. Toolchain file is expected to have the same name
 build_bdwgc()
 {
     BUILD_DIR=bdwgc_build
@@ -14,21 +22,55 @@ build_bdwgc()
                 -Denable_dynamic_loading=OFF"
 
 
+    echo "Building bdwgc library for CI/CD"
     mkdir -p ${BUILD_DIR}
-    cmake -B ${BUILD_DIR} -S . ${BUILD_OPTS} -DCMAKE_TOOLCHAIN_FILE=${1}
+    cmake -B ${BUILD_DIR} -S . ${BUILD_OPTS} -DCMAKE_TOOLCHAIN_FILE=${1}.cmake
     cmake --build ${BUILD_DIR}
+    cmake --install ${BUILD_DIR}
 }
 
-clean_bdwgc()
+build_bdwgc_clients()
 {
-    BUILD_DIR=bdwgc_build
+    SRC_DIR=ci/tests
+    BUILD_DIR=bdwgc_client_build
     INSTALL_DIR=bdwgc_install
-    rm -rf ${BUILD_DIR}
-    rm -rf ${INSTALL_DIR}
+    BUILD_OPTS="-DCMAKE_BUILD_TYPE=Debug \
+                -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
+
+    echo "Building test clients for CI/CD"
+    mkdir -p ${BUILD_DIR}
+    cmake -B ${BUILD_DIR} -S ${SRC_DIR} ${BUILD_OPTS} -DCMAKE_TOOLCHAIN_FILE=../../${1}.cmake
+    cmake --build ${BUILD_DIR}
+    cmake --install ${BUILD_DIR}
 }
 
-clean_bdwgc
-build_bdwgc riscv64-purecap.cmake
-clean_bdwgc
-build_bdwgc morello-purecap.cmake
-clean_bdwgc
+
+clean()
+{
+    rm -rf ${@}
+}
+
+echo "Checking bdwgc library builds correctly"
+build_bdwgc ${PLATFORMS}
+echo "Checking cheri bdwgc clients build correctly"
+build_bdwgc_clients ${PLATFORMS}
+
+echo "Running tests for riscv64-purecap"
+args=(
+    --architecture riscv64-purecap
+    # Qemu System to use
+    --qemu-cmd $HOME/cheri/output/sdk/bin/qemu-system-riscv64cheri
+    # Kernel (to avoid the default one) 
+    --kernel $HOME/cheri/output/rootfs-riscv64-purecap/boot/kernel/kernel
+    # Bios (to avoid the default one) 
+    --bios bbl-riscv64cheri-virt-fw_jump.bin
+    --disk-image $HOME/cheri/output/cheribsd-riscv64-purecap.img
+    # Required build-dir in CheriBSD
+    --build-dir .
+    --ssh-port $SSHPORT
+    --ssh-key $HOME/.ssh/id_ed25519.pub
+    )
+BUILDBOT_PLATFORM=riscv64-purecap python3 ci/run_cheri_bdwgc_tests.py "${args[@]}"
+
+echo "removing up unit-test files" 
+clean bdwgc_build bdwgc_client_build bdwgc_install
---
+++ ci/run_tests.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 The CapableVMs "CHERI Examples" Contributors.
+# SPDX-License-Identifier: MIT OR Apache-2.0
+#
+# Test that examples execute as expected.
+#
+# This is designed to be run by .buildbot.sh, but can also be run manually too,
+# as long as you have a suitable environment, and an execution platform set up
+# and running so that the make-based run-* targets have something to connect to.
+# Notable environment variables:
+#
+# - BUILDBOT_PLATFORM
+#   - Used to compile with the correct toolchain. Set to something like
+#     "morello-purecap" or "riscv64-purecap".
+# - SSHPORT
+# - SSHUSER (defaults to 'root')
+# - SSHHOST (defaults to 'localhost')
+#
+# TODO: Extend this to cover interactive examples and other architectures. 
+# For now, we just test examples on `morello-hybrid`.
+
+set -e
+
+ERROR=false
+if [ -z "$SSHPORT" ]; then
+    echo "SSHPORT is empty or not set" >&2
+    ERROR=true
+fi
+if [ -z "$BUILDBOT_PLATFORM" ]; then
+    echo "BUILDBOT_PLATFORM is empty or not set" >&2
+    ERROR=true
+fi
+$ERROR && exit 1
+
+# One time setup per architecture
+setup() 
+{
+    # Copy library files to target VM
+    scp -o "StrictHostKeyChecking no" -P ${SSHPORT} bdwgc_install/lib/libgc.so.1.5.0 bdwgc_install/lib/libcord.so.1.4.0 root@${SSHHOST}:/root
+    if [ $? -ne 0 ]; then 
+        echo "Error copying bdwgc library to ${BUILDBOT_PLATFORM} VM"
+        exit 1
+    fi 	
+    # Ensure linker in the VM can find the copied bdwgc libraries 
+    ssh -o "StrictHostKeyChecking no" -p ${SSHPORT} root@${SSHHOST} -t 'ln -fs libgc.so.1 libgc.so \
+                                                     && ln -fs libgc.so.1.5.0 libgc.so.1 \
+						     && ln -fs libcord.so.1 libcord.so \
+						     && ln -fs libcord.so.1.4.0 libcord.so.1'
+    if [ $? -ne 0 ]; then 
+        echo "Error creating links for bdwgc library within ${BUILDBOT_PLATFORM} VM execution environment"
+        exit 1
+    fi 	
+}
+
+# arg-1 : compiled unit-test executable (with relative path)
+run()
+{
+    for name in "${@:2}"; do
+        echo "Transferring  ${name}"
+        scp -o "StrictHostKeyChecking no" -P ${SSHPORT} ${name}  root@${SSHHOST}:/root
+        if [ $? -ne 0 ]; then 
+            echo "Error copying unit-test executable ${name} to ${BUILDBOT_PLATFORM} VM"
+            exit 1
+        fi 	
+
+        # These tests should trigger an "In-address space security exception"
+        # So they should fail, i.e. `exit=1`
+        if [ "$1" = "to_fail" ]; then
+            exit_status=0
+            RESULT={{$(ssh -o "StrictHostKeyChecking no" -p ${SSHPORT} root@${SSHHOST} -t \
+	                   "setenv LD_LIBRARY_PATH /root && ./$(basename ${name})")} && exit_status=1} || true
+            if [ $exit_status != 0 ]; then
+                exit 1
+            fi
+        else
+            ssh -o "StrictHostKeyChecking no" -p ${SSHPORT} root@${SSHHOST} -t \
+	                   "setenv LD_LIBRARY_PATH /root && ./$(basename ${name})"
+        fi
+    done
+
+}
+
+# Copy bdwgc library and set up environment 
+setup
+
+# Execute 
+run OK "bdwgc_install/bin/small_fixed_alloc.elf"
+run to_fail "bdwgc_install/bin/random_mixed_alloc.elf"
