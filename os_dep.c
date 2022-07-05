66cb7fc2a8cb1f3b45e4bcbd9bf6e71354380b94
Ivan Maidanski <ivmai@mail.ru>
2022-07-05 21:12:58 +0300

    Revert "Remove nested always-false ifdef for HPUX and FREEBSD"
    This reverts commit 2a420e272d9f036130fbf95a359fb4140ff11da3.
    
    The reason: SUNOS5SIGS is defined for HP/UX and some flavors of
    FreeBSD.

--- os_dep.c
+++ os_dep.c
@@ -875,7 +875,7 @@ GC_INNER size_t GC_page_size = 0;
        || defined(HAIKU) || defined(HURD) || defined(FREEBSD) \
        || defined(NETBSD)
         static struct sigaction old_segv_act;
-#       if defined(_sigargs) /* !Irix6.x */ \
+#       if defined(_sigargs) /* !Irix6.x */ || defined(HPUX) \
            || defined(HURD) || defined(NETBSD) || defined(FREEBSD)
             static struct sigaction old_bus_act;
 #       endif
@@ -912,7 +912,8 @@ GC_INNER size_t GC_page_size = 0;
 #         else
             (void) sigaction(SIGSEGV, &act, &old_segv_act);
 #           if defined(IRIX5) && defined(_sigargs) /* Irix 5.x, not 6.x */ \
-               || defined(HURD) || defined(NETBSD) || defined(FREEBSD)
+               || defined(HPUX) || defined(HURD) || defined(NETBSD) \
+               || defined(FREEBSD)
               /* Under Irix 5.x or HP/UX, we may get SIGBUS.    */
               /* Pthreads doesn't exist under Irix 5.x, so we   */
               /* don't have to worry in the threads case.       */
@@ -958,7 +959,8 @@ GC_INNER size_t GC_page_size = 0;
            || defined(NETBSD)
           (void) sigaction(SIGSEGV, &old_segv_act, 0);
 #         if defined(IRIX5) && defined(_sigargs) /* Irix 5.x, not 6.x */ \
-             || defined(HURD) || defined(NETBSD)
+             || defined(HPUX) || defined(HURD) || defined(NETBSD) \
+             || defined(FREEBSD)
               (void) sigaction(SIGBUS, &old_bus_act, 0);
 #         endif
 #       else
