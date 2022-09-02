e7504253477e0e6f489c500eb16160f60f34afa9
Lucas Holt <luke@foolishgames.com>
2022-09-02 09:04:08 +0300

    Fix IRIX5 defined wrongly on FreeBSD/mips (a cherry-pick of commit f1a855f08 from 'master')
    Issue #471 (bdwgc).
    
    * include/private/gcconfig.h [(mips || __mips || _mips) && !LINUX
    && !EWS4800 && !NETBSD] (IRIX5): Do not define if FREEBSD.

--- include/private/gcconfig.h
+++ include/private/gcconfig.h
@@ -197,7 +197,7 @@
 #      define EWS4800
 #    endif
 #    if !defined(LINUX) && !defined(EWS4800) && !defined(NETBSD) \
-        && !defined(OPENBSD)
+        && !defined(OPENBSD) && !defined(FREEBSD)
 #      if defined(ultrix) || defined(__ultrix)
 #        define ULTRIX
 #      else
