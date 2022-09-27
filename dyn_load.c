ed081c23858e03e2ad62ffe59e407fb7d43b2d3d
Ivan Maidanski <ivmai@mail.ru>
2022-09-27 08:52:42 +0300

    Fix a typo in comment of dyn_load.c (shortened USE_PROC_FOR_LIBRARIES)
    * dyn_load.c [SOLARISDL || !LINUX]: Rename USE_PROC to
    USE_PROC_FOR_LIBRARIES in comment.

--- dyn_load.c
+++ dyn_load.c
@@ -258,7 +258,7 @@ GC_INNER void GC_register_dynamic_libraries(void)
     }
 }
 
-# endif /* !USE_PROC ... */
+# endif /* !USE_PROC_FOR_LIBRARIES */
 # endif /* SOLARISDL */
 
 #if defined(SCO_ELF) || defined(DGUX) || defined(HURD) || defined(NACL) \
@@ -932,7 +932,7 @@ GC_INNER void GC_register_dynamic_libraries(void)
         fd = -1;
 }
 
-# endif /* USE_PROC || IRIX5 */
+# endif /* USE_PROC_FOR_LIBRARIES || IRIX5 */
 
 # if defined(MSWIN32) || defined(MSWINCE) || defined(CYGWIN32)
 
