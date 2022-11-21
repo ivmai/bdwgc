f29efafc4a8416d0d975c7c3bd1a372448899bfe
Ivan Maidanski <ivmai@mail.ru>
2022-11-22 00:39:48 +0300

    Provide meaningful error message in case of Emscripten threaded build (a cherry-pick of commit 11253c7cd from 'master')
    Issue #503 (bdwgc).
    
    WebAssembly/Web workers do not currently/yet support synchronous
    stopping of all threads, so disallow building in multi-threaded mode
    with Emscripten, and hint the user to build single-threaded bdwgc
    instead.
    
    * include/private/gcconfig.h [EMSCRIPTEN && GC_THREADS && !CPPCHECK]:
    Report an error (that the multi-threaded build is not supported) using
    a preprocessor directive.
    
    Co-authored-by: Jukka Jylanki <jujjyl@gmail.com>

--- include/private/gcconfig.h
+++ include/private/gcconfig.h
@@ -1427,6 +1427,9 @@ EXTERN_C_BEGIN
 #     define DATAEND (ptr_t)ALIGNMENT
 #     define USE_MMAP_ANON      /* avoid /dev/zero, not supported */
 #     define STACK_GROWS_DOWN
+#     if defined(GC_THREADS) && !defined(CPPCHECK)
+#       error No threads support yet
+#     endif
 #   endif
 #   if defined(__QNX__)
 #     define OS_TYPE "QNX"
