2d5f2368d04231756bfc935901db20fbfece1d5f
Ivan Maidanski <ivmai@mail.ru>
2023-01-01 16:20:09 +0300

    Fix 'ISO C90 forbids mixed decl and code' warning in SOFT_VDB dirty_init (a cherry-pick of commit c934a428c from 'master')
    Issue #265 (bdwgc).
    
    * os_dep.c [SOFT_VDB && MPROTECT_VDB] (GC_dirty_init): Move assertion
    about soft_vdb_buf down (to be after char *str).

--- os_dep.c
+++ os_dep.c
@@ -3907,9 +3907,9 @@ GC_INLINE void GC_proc_read_dirty(GC_bool output_unneeded)
     GC_INNER GC_bool GC_dirty_init(void)
 # endif
   {
-    GC_ASSERT(NULL == soft_vdb_buf);
 #   ifdef MPROTECT_VDB
       char * str = GETENV("GC_USE_GETWRITEWATCH");
+
 #     ifdef GC_PREFER_MPROTECT_VDB
         if (str == NULL || (*str == '0' && *(str + 1) == '\0'))
           return FALSE; /* the environment variable is unset or set to "0" */
@@ -3918,6 +3918,7 @@ GC_INLINE void GC_proc_read_dirty(GC_bool output_unneeded)
           return FALSE; /* the environment variable is set "0" */
 #     endif
 #   endif
+    GC_ASSERT(NULL == soft_vdb_buf);
 #   ifndef NO_SOFT_VDB_LINUX_VER_RUNTIME_CHECK
       if (!ensure_min_linux_ver(3, 18)) {
         GC_COND_LOG_PRINTF(
