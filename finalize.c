5fbcdd29b8a98fffdc6b9bcf517e49a0221b32bb
Ivan Maidanski <ivmai@mail.ru>
2023-02-08 22:17:28 +0300

    Fix GC_unreachable_finalize_mark_proc to ensure its unique address
    Even GC_unreachable_finalize_mark_proc and GC_normal_finalize_mark_proc
    have identical behavior, their addresses should be guaranteed to be
    distinct despite any potential compiler optimization.
    
    * finalize.c (GC_unreachable_finalize_mark_proc): Add a dummy statement
    before GC_normal_finalize_mark_proc() call; add comment.

--- finalize.c
+++ finalize.c
@@ -647,6 +647,11 @@ STATIC void GC_null_finalize_mark_proc(ptr_t p GC_ATTR_UNUSED) {}
 /* other objects specify no ordering.                                   */
 STATIC void GC_unreachable_finalize_mark_proc(ptr_t p)
 {
+    /* A dummy comparison to ensure the compiler not to optimize two    */
+    /* identical functions into a single one (thus, to ensure a unique  */
+    /* address of each).  Alternatively, GC_noop1(p) could be used.     */
+    if (EXPECT(NULL == p, FALSE)) return;
+
     GC_normal_finalize_mark_proc(p);
 }
 
