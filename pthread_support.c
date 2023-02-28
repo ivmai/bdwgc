1a8160ea8e3aec6e673dc1cd121cde60d5c7f396
Ivan Maidanski <ivmai@mail.ru>
2023-02-28 09:49:53 +0300

    Fix a typo in comment of GC_start_rtn_prepare_thread (fix of commit 579101b33)
    * pthread_support.c [GC_PTHREADS && !SN_TARGET_ORBIS
    && !SN_TARGET_PSP2] (GC_start_rtn_prepare_thread): Fix a typo in
    comment ("used in").

--- pthread_support.c
+++ pthread_support.c
@@ -2361,7 +2361,7 @@ GC_API int GC_CALL GC_register_my_thread(const struct GC_stack_base *sb)
 
   /* Called from GC_pthread_start_inner().  Defined in this file to     */
   /* minimize the number of include files in pthread_start.c (because   */
-  /* sem_t and sem_post() are not used that file directly).             */
+  /* sem_t and sem_post() are not used in that file directly).          */
   GC_INNER_PTHRSTART GC_thread GC_start_rtn_prepare_thread(
                                         void *(**pstart)(void *),
                                         void **pstart_arg,
