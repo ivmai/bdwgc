9d419cb7b50d1bc750ed4de0112d6ad9f160d5c4
Ivan Maidanski <ivmai@mail.ru>
2023-02-28 09:49:53 +0300

    Fix a typo in comment of GC_start_rtn_prepare_thread (a cherry-pick of commit 0be9aa88e from 'release-7_4')
    * pthread_support.c (GC_start_rtn_prepare_thread): Fix a typo in
    comment ("used in").

--- pthread_support.c
+++ pthread_support.c
@@ -1544,7 +1544,7 @@ struct start_info {
 
 /* Called from GC_inner_start_routine().  Defined in this file to       */
 /* minimize the number of include files in pthread_start.c (because     */
-/* sem_t and sem_post() are not used that file directly).               */
+/* sem_t and sem_post() are not used in that file directly).            */
 GC_INNER GC_thread GC_start_rtn_prepare_thread(void *(**pstart)(void *),
                                         void **pstart_arg,
                                         struct GC_stack_base *sb, void *arg)
