c918927c75bd314653524b46dd485c2c87f826ff
Ivan Maidanski <ivmai@mail.ru>
2023-05-26 00:19:14 +0300

    [7.4.24]
    Bump gc version to 7.4.24
    
    * ChangeLog (7.4.24): Set release date.
    * Makefile.am (LIBGC_VER_INFO): Increment revision (change version
    info of libgc.so to 3:8:2).
    * README.md: Bump micro version (to 7.4.24).
    * configure.ac (AC_INIT): Likewise.
    * include/gc_version.h (GC_TMP_VERSION_MICRO): Likewise.

--- include/gc_version.h
+++ include/gc_version.h
@@ -30,7 +30,7 @@
 /* it to keep the old-style build process working.              */
 #define GC_TMP_VERSION_MAJOR 7
 #define GC_TMP_VERSION_MINOR 4
-#define GC_TMP_VERSION_MICRO 22 /* 7.4.22 */
+#define GC_TMP_VERSION_MICRO 24 /* 7.4.24 */
 
 #ifdef GC_VERSION_MAJOR
 # if GC_TMP_VERSION_MAJOR != GC_VERSION_MAJOR \
