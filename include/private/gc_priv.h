f95a4527b0814eb5b6b1791a34bf1a378ec450ce
Ivan Maidanski <ivmai@mail.ru>
2023-06-19 21:01:42 +0300

    Fix a typo in comment of SPARC-specific SIG_SUSPEND definition
    * include/private/gc_priv.h [(GC_LINUX_THREADS || GC_DGUX386_THREADS)
    && !GC_USESIGRT_SIGNALS && SPARC && !SIGPWR] (SIG_SUSPEND): Fix a typo
    ("Linux/SPARC") in comment.

--- include/private/gc_priv.h
+++ include/private/gc_priv.h
@@ -2585,7 +2585,7 @@ GC_INNER void *GC_store_debug_info_inner(void *p, word sz, const char *str,
 # if (defined(GC_LINUX_THREADS) || defined(GC_DGUX386_THREADS)) \
      && !defined(GC_USESIGRT_SIGNALS)
 #   if defined(SPARC) && !defined(SIGPWR)
-      /* SPARC/Linux doesn't properly define SIGPWR in <signal.h>.      */
+      /* Linux/SPARC doesn't properly define SIGPWR in <signal.h>.      */
       /* It is aliased to SIGLOST in asm/signal.h, though.              */
 #     define SIG_SUSPEND SIGLOST
 #   else
