bfb3680a45c82a58557cb8911f65f9c9a31299ad
Ivan Maidanski <ivmai@mail.ru>
2023-08-01 09:44:36 +0300

    Define GC_GRANULE_BYTES macro based on GC_GRANULE_WORDS value (refactoring)
    * include/gc/gc_tiny_fl.h: Reformat comments.
    * include/gc/gc_tiny_fl.h [!GC_GRANULE_BYTES]: Reformat code.
    * include/gc/gc_tiny_fl.h (GC_GRANULE_WORDS): Define unconditionally.
    * include/gc/gc_tiny_fl.h [!GC_GRANULE_BYTES] (GC_GRANULE_BYTES):
    Define based on GC_GRANULE_WORDS.

--- include/gc/gc_tiny_fl.h
+++ include/gc/gc_tiny_fl.h
@@ -13,49 +13,45 @@
 
 #ifndef GC_TINY_FL_H
 #define GC_TINY_FL_H
-/*
- * Constants and data structures for "tiny" free lists.
- * These are used for thread-local allocation or in-lined allocators.
- * Each global free list also essentially starts with one of these.
- * However, global free lists are known to the GC.  "Tiny" free lists
- * are basically private to the client.  Their contents are viewed as
- * "in use" and marked accordingly by the core of the GC.
- *
- * Note that inlined code might know about the layout of these and the constants
- * involved.  Thus any change here may invalidate clients, and such changes should
- * be avoided.  Hence we keep this as simple as possible.
- */
 
-/*
- * We always set GC_GRANULE_BYTES to twice the length of a pointer.
- * This means that all allocation requests are rounded up to the next
- * multiple of 16 on 64-bit architectures or 8 on 32-bit architectures.
- * This appears to be a reasonable compromise between fragmentation overhead
- * and space usage for mark bits (usually mark bytes).
- * On many 64-bit architectures some memory references require 16-byte
- * alignment, making this necessary anyway.
- * For a few 32-bit architecture (e.g. x86), we may also need 16-byte alignment
- * for certain memory references.  But currently that does not seem to be the
- * default for all conventional malloc implementations, so we ignore that
- * problem.
- * It would always be safe, and often useful, to be able to allocate very
- * small objects with smaller alignment.  But that would cost us mark bit
- * space, so we no longer do so.
- */
+/* Constants and data structures for "tiny" free lists.  These are      */
+/* used for thread-local allocation or in-lined allocators.             */
+/* Each global free list also essentially starts with one of these.     */
+/* However, global free lists are known to the GC.  "Tiny" free lists   */
+/* are basically private to the client.  Their contents are viewed as   */
+/* "in use" and marked accordingly by the core of the GC.               */
+/* Note that inlined code might know about the layout of these and the  */
+/* constants involved.  Thus any change here may invalidate clients,    */
+/* and such changes should be avoided.  Hence we keep this as simple    */
+/* as possible.                                                         */
+
+/* We always set GC_GRANULE_BYTES to twice the length of a pointer.     */
+/* This means that all allocation requests are rounded up to the next   */
+/* multiple of 16 on 64-bit architectures or 8 on 32-bit architectures. */
+/* This appears to be a reasonable compromise between fragmentation     */
+/* overhead and space usage for mark bits (usually mark bytes).         */
+/* On many 64-bit architectures some memory references require 16-byte  */
+/* alignment, making this necessary anyway.  For a few 32-bit           */
+/* architectures (e.g. x86), we may also need 16-byte alignment for     */
+/* certain memory references.  But currently that does not seem to be   */
+/* the default for all conventional malloc implementations, so we       */
+/* ignore that problem.                                                 */
+/* It would always be safe, and often useful, to be able to allocate    */
+/* very small objects with smaller alignment.  But that would cost us   */
+/* mark bit space, so we no longer do so.                               */
+
+/* GC_GRANULE_BYTES should not be overridden in any instances of the GC */
+/* library that may be shared between applications, since it affects    */
+/* the binary interface to the library.                                 */
 #ifndef GC_GRANULE_BYTES
-  /* GC_GRANULE_BYTES should not be overridden in any instances of the GC */
-  /* library that may be shared between applications, since it affects    */
-  /* the binary interface to the library.                                 */
+# define GC_GRANULE_WORDS 2
 # if defined(__LP64__) || defined (_LP64) || defined(_WIN64) \
-        || defined(__s390x__) \
-        || (defined(__x86_64__) && !defined(__ILP32__)) \
-        || defined(__alpha__) || defined(__powerpc64__) \
-        || defined(__arch64__)
-#   define GC_GRANULE_BYTES 16
-#   define GC_GRANULE_WORDS 2
+        || defined(__alpha__) || defined(__arch64__) \
+        || defined(__powerpc64__) || defined(__s390x__) \
+        || (defined(__x86_64__) && !defined(__ILP32__))
+#   define GC_GRANULE_BYTES (GC_GRANULE_WORDS * 8 /* sizeof(void*) */)
 # else
-#   define GC_GRANULE_BYTES 8
-#   define GC_GRANULE_WORDS 2
+#   define GC_GRANULE_BYTES (GC_GRANULE_WORDS * 4 /* sizeof(void*) */)
 # endif
 #endif /* !GC_GRANULE_BYTES */
 
