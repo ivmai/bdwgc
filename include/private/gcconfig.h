07b0551baaab8a85405383e17a14b195bf255eb6
Ivan Maidanski <ivmai@mail.ru>
2023-08-28 08:40:29 +0300

    Fix 'implicit declaration of function sbrk' gcc error on Symbian (a cherry-pick of commit 3cd72c1b8 from 'release-8_2')
    Defining USE_MMAP macro is mandatory when compiling for Symbian
    because sbrk() is not provided there.
    
    * include/private/gcconfig.h [ARM32 && SYMBIAN && !USE_MMAP]
    (USE_MMAP): Define.

--- include/private/gcconfig.h
+++ include/private/gcconfig.h
@@ -2480,6 +2480,10 @@ EXTERN_C_BEGIN
 #     define ALIGNMENT 4
 #     define DATASTART (ptr_t)ALIGNMENT /* cannot be null */
 #     define DATAEND (ptr_t)ALIGNMENT
+#     ifndef USE_MMAP
+        /* sbrk() is not available. */
+#       define USE_MMAP 1
+#     endif
 #   endif
 #   ifdef NOSYS
       /* __data_start is usually defined in the target linker script.  */
