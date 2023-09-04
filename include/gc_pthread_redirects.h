caa34029f7776b78eda52cc02e39b8c6f5407f8a
Ivan Maidanski <ivmai@mail.ru>
2023-09-04 08:41:31 +0300

    Fix missing GC_pthread_sigmask on Free/NetBSD (a cherry-pick of commit 3ed9b96eb from 'master')
    Issue #571 (bdwgc).
    
    * include/gc_pthread_redirects.h [!GC_NO_PTHREAD_SIGMASK
    && __POSIX_VISIBLE>=199506] (GC_pthread_sigmask): Declare.
    * include/gc_pthread_redirects.h [!GC_NO_PTHREAD_SIGMASK
    && !(GC_PTHREAD_SIGMASK_NEEDED || _BSD_SOURCE || _GNU_SOURCE
    || _POSIX_C_SOURCE>=199506L || _XOPEN_SOURCE>=500)]
    (GC_NO_PTHREAD_SIGMASK): Do not define if __POSIX_VISIBLE>=199506.

--- include/gc_pthread_redirects.h
+++ include/gc_pthread_redirects.h
@@ -46,7 +46,8 @@
 #   include <signal.h>  /* needed anyway for proper redirection */
 #   if defined(GC_PTHREAD_SIGMASK_NEEDED) \
         || defined(_BSD_SOURCE) || defined(_GNU_SOURCE) \
-        || (_POSIX_C_SOURCE >= 199506L) || (_XOPEN_SOURCE >= 500)
+        || (_POSIX_C_SOURCE >= 199506L) || (_XOPEN_SOURCE >= 500) \
+        || (__POSIX_VISIBLE >= 199506) /* xBSD internal macro */
       GC_API int GC_pthread_sigmask(int /* how */, const sigset_t *,
                                     sigset_t * /* oset */);
 #   else
