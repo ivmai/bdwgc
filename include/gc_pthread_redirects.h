592952ba2d06c23b421e99ecfd8c529686c6fd8c
Ivan Maidanski <ivmai@mail.ru>
2023-09-11 20:12:50 +0300

    Fix missing GC_pthread_sigmask on NetBSD (a cherry-pick of commit 36e6697b8 from 'release-8_0')
    Issue #571 (bdwgc).
    
    * include/gc_pthread_redirects.h [!GC_NO_PTHREAD_SIGMASK
    && _NETBSD_SOURCE] (GC_pthread_sigmask): Declare.
    * include/gc_pthread_redirects.h [!GC_NO_PTHREAD_SIGMASK
    && !(GC_PTHREAD_SIGMASK_NEEDED || _BSD_SOURCE || _GNU_SOURCE
    || _POSIX_C_SOURCE>=199506L || _XOPEN_SOURCE>=500
    || __POSIX_VISIBLE>=199506)] (GC_NO_PTHREAD_SIGMASK): Do not define
    if _NETBSD_SOURCE.

--- include/gc_pthread_redirects.h
+++ include/gc_pthread_redirects.h
@@ -40,8 +40,8 @@
 
 # ifndef GC_NO_PTHREAD_SIGMASK
 #   include <signal.h>  /* needed anyway for proper redirection */
-#   if defined(GC_PTHREAD_SIGMASK_NEEDED) \
-        || defined(_BSD_SOURCE) || defined(_GNU_SOURCE) \
+#   if defined(GC_PTHREAD_SIGMASK_NEEDED) || defined(_BSD_SOURCE) \
+        || defined(_GNU_SOURCE) || defined(_NETBSD_SOURCE) \
         || (_POSIX_C_SOURCE >= 199506L) || (_XOPEN_SOURCE >= 500) \
         || (__POSIX_VISIBLE >= 199506) /* xBSD internal macro */
       GC_API int GC_pthread_sigmask(int /* how */, const sigset_t *,
