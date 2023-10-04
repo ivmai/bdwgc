6594e9a7b0cac3e876510759a1b80b26f76212e2
Ivan Maidanski <ivmai@mail.ru>
2023-10-04 23:40:42 +0300

    Fix handling of GC_gc_no counter wrap in GC_notify_or_invoke_finalizers
    * finalize.c [KEEP_BACK_PTRS] (GC_notify_or_invoke_finalizers):
    Compare GC_gc_no and last_back_trace_gc_no values using inequality
    operator (instead of greater-than); define bt_in_progress static
    variable; if bt_in_progress then do not do print any backtraces, else
    set bt_in_progress (instead of setting last_back_trace_gc_no to
    GC_WORD_MAX) while printing the backtraces; remove comment about
    wrap of GC_gc_no counter.

--- finalize.c
+++ finalize.c
@@ -1342,12 +1342,14 @@ GC_INNER void GC_notify_or_invoke_finalizers(void)
     /* This is a convenient place to generate backtraces if appropriate, */
     /* since that code is not callable with the allocator lock.          */
 #   if defined(KEEP_BACK_PTRS) || defined(MAKE_BACK_GRAPH)
-      if (GC_gc_no > last_back_trace_gc_no) {
+      if (GC_gc_no != last_back_trace_gc_no) {
 #       ifdef KEEP_BACK_PTRS
+          static GC_bool bt_in_progress = FALSE;
           long i;
-          /* Stops when GC_gc_no wraps; that's OK.      */
-          last_back_trace_gc_no = GC_WORD_MAX;  /* disable others. */
-          for (i = 0; i < GC_backtraces; ++i) {
+
+          if (!bt_in_progress) {
+            bt_in_progress = TRUE; /* prevent recursion or parallel usage */
+            for (i = 0; i < GC_backtraces; ++i) {
               /* FIXME: This tolerates concurrent heap mutation, which  */
               /* may cause occasional mysterious results.  We need to   */
               /* release the allocator lock, since GC_print_callers()   */
@@ -1358,6 +1360,8 @@ GC_INNER void GC_notify_or_invoke_finalizers(void)
               GC_printf("\n****Chosen address %p in object\n", current);
               GC_print_backtrace(current);
               LOCK();
+            }
+            bt_in_progress = FALSE;
           }
 #       endif
         last_back_trace_gc_no = GC_gc_no;
