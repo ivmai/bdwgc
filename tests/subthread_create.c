620d4a180662550fb593aca4c4b80a5eb8d7ef65
Ivan Maidanski <ivmai@mail.ru>
2023-12-08 23:14:49 +0300

    Fix thread_id variable definition placement in main of subthreadcreatetest (fix of commit a3df481b8)
    * tests/subthreadcreate.c [AO_HAVE_fetch_and_add1 && NTHREADS>0
    && !GC_PTHREADS] (main): Make thread_id local variable to be defined
    at the beginning of a block statement.

--- tests/subthread_create.c
+++ tests/subthread_create.c
@@ -127,8 +127,11 @@ int main(void)
             exit(1);
         }
 #     else
-        DWORD thread_id;
-        th[i] = CreateThread(NULL, 0, entry, 0, 0, &thread_id);
+        {
+          DWORD thread_id;
+
+          th[i] = CreateThread(NULL, 0, entry, 0, 0, &thread_id);
+        }
         if (th[i] == NULL) {
             fprintf(stderr, "Thread creation failed, errcode= %d\n",
                     (int)GetLastError());
@@ -140,6 +143,7 @@ int main(void)
     for (i = 0; i < n; ++i) {
 #     ifdef GC_PTHREADS
         void *res;
+
         err = pthread_join(th[i], &res);
         if (err) {
             fprintf(stderr, "Failed to join thread, error: %s\n",
