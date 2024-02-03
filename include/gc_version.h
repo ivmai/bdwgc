b5b795adb009140d4d41b97792b42b3c1d4d487f
Ivan Maidanski <ivmai@mail.ru>
2024-02-03 19:27:41 +0300

    [7.6.20]
    Bump gc version to 7.6.20
    
    * ChangeLog (7.6.20): Set release date.
    * Makefile.am (LIBGC_VER_INFO): Increment revision (change version info
    of libgc.so to 4:10:3).
    * README.md: Bump micro version.
    * configure.ac (AC_INIT): Likewise.
    * include/gc_version.h (GC_TMP_VERSION_MICRO): Likewise.

--- include/gc_version.h
+++ include/gc_version.h
@@ -30,7 +30,7 @@
 /* it to keep the old-style build process working.              */
 #define GC_TMP_VERSION_MAJOR 7
 #define GC_TMP_VERSION_MINOR 6
-#define GC_TMP_VERSION_MICRO 18 /* 7.6.18 */
+#define GC_TMP_VERSION_MICRO 20 /* 7.6.20 */
 
 #ifdef GC_VERSION_MAJOR
 # if GC_TMP_VERSION_MAJOR != GC_VERSION_MAJOR \
