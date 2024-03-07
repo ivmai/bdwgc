be71c12faab9d8985c0171801202d5dc9d64a73a
Jake Hughes <jh@jakehughes.uk>
2024-03-07 11:32:50 +0000

    Add noinline attribute

--- allchblk.c
+++ allchblk.c
@@ -1008,7 +1008,7 @@ STATIC struct hblk *GC_allochblk_nth(size_t lb_adjusted, int k,
 # ifdef VALGRIND_TRACKING
 /* This function is called from the sweeper whenever an object is      */
 /* freed.                                                              */
-    GC_API void GC_CALL GC_free_profiler_hook(void * p) {
+    GC_ATTR_NOINLINE GC_API void GC_CALL GC_free_profiler_hook(void * p) {
         GC_noop1((word)(p));
     }
 # endif
