206aefb467490e4caeb981d006d8e901cc60bbc5
Jake Hughes <jh@jakehughes.uk>
2024-03-12 18:00:12 +0000

    Remove blankline

--- malloc.c
+++ malloc.c
@@ -650,11 +650,9 @@ GC_API void GC_CALL GC_free(void * p)
         /* Required by ANSI.  It's not my fault ...     */
         return;
     }
-
 #   ifdef VALGRIND_TRACKING
       GC_free_profiler_hook(p);
 #   endif
-
 #   ifdef LOG_ALLOCS
       GC_log_printf("GC_free(%p) after GC #%lu\n",
                     p, (unsigned long)GC_gc_no);
