be5351381e5044dfcf2a423e34c1147787312262
Ivan Maidanski <ivmai@mail.ru>
2024-03-17 18:02:52 +0300

    Prevent redirected malloc call from a garbage collection routine
    Issue #612 (bdwgc).
    
    Do not support call stack saving by backtrace() at garbage collection
    in case of malloc redirection because backtrace() may call malloc().
    
    * include/private/gc_priv.h [SAVE_CALL_CHAIN
    && !DONT_SAVE_TO_LAST_STACK] (GC_arrays._last_stack): Do not define
    if REDIRECT_MALLOC and GC_HAVE_BUILTIN_BACKTRACE; update comment.
    * include/private/gc_priv.h [SAVE_CALL_CHAIN && REDIRECT_MALLOC
    && GC_HAVE_BUILTIN_BACKTRACE && !DONT_SAVE_TO_LAST_STACK]
    (SAVE_CALLERS_TO_LAST_STACK): Define to no-op.

--- include/private/gc_priv.h
+++ include/private/gc_priv.h
@@ -1339,12 +1339,14 @@ struct _GC_arrays {
 #   define GC_root_index GC_arrays._root_index
     struct roots * _root_index[RT_SIZE];
 # endif
-# if defined(SAVE_CALL_CHAIN) && !defined(DONT_SAVE_TO_LAST_STACK)
+# if defined(SAVE_CALL_CHAIN) && !defined(DONT_SAVE_TO_LAST_STACK) \
+     && (!defined(REDIRECT_MALLOC) || !defined(GC_HAVE_BUILTIN_BACKTRACE))
     struct callinfo _last_stack[NFRAMES];
                 /* Stack at last garbage collection.  Useful for        */
                 /* debugging mysterious object disappearances.  In the  */
                 /* multi-threaded case, we currently only save the      */
-                /* calling stack.                                       */
+                /* calling stack.  Not supported in case of malloc      */
+                /* redirection because backtrace() may call malloc().   */
 #   define SAVE_CALLERS_TO_LAST_STACK() GC_save_callers(GC_arrays._last_stack)
 # else
 #   define SAVE_CALLERS_TO_LAST_STACK() (void)0
