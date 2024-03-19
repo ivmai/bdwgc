36a48ec8ad345c45c25a28aac164770630fc88cb
Ivan Maidanski <ivmai@mail.ru>
2024-03-19 08:07:03 +0300

    Fix ADD_CALL_CHAIN() placement to follow GC_store_debug_info_inner call (a cherry-pick of commit 70163b7da from 'release-8_0')
    Issue #613 (bdwgc).
    
    This might be important for the case of malloc redirection as
    backtrace() need to be called without the allocator lock held.
    
    * dbg_mlc.c (store_debug_info, GC_debug_generic_malloc_inner,
    GC_debug_generic_malloc_inner_ignore_off_page): Call ADD_CALL_CHAIN()
    right after GC_store_debug_info_inner() call (instead of before it).
    * gcj_mlc.c (GC_debug_gcj_malloc): Likewise.
    * dbg_mlc.c (GC_debug_generic_malloc_inner,
    GC_debug_generic_malloc_inner_ignore_off_page): Define base local
    variable.
    * gcj_mlc.c (GC_debug_gcj_malloc): Likewise.

--- dbg_mlc.c
+++ dbg_mlc.c
@@ -293,8 +293,8 @@ static void *store_debug_info(void *p, size_t lb,
     LOCK();
     if (!GC_debugging_started)
         GC_start_debugging_inner();
-    ADD_CALL_CHAIN(p, ra);
     result = GC_store_debug_info_inner(p, (word)lb, s, i);
+    ADD_CALL_CHAIN(p, ra);
     UNLOCK();
     return result;
 }
@@ -552,10 +552,11 @@ GC_API GC_ATTR_MALLOC void * GC_CALL
   /* We assume we already hold the GC lock.                             */
   GC_INNER void * GC_debug_generic_malloc_inner(size_t lb, int k)
   {
-    void * result = GC_generic_malloc_inner(
+    void *base = GC_generic_malloc_inner(
                                 SIZET_SAT_ADD(lb, DEBUG_BYTES), k);
+    void *result;
 
-    if (result == 0) {
+    if (NULL == base) {
         GC_err_printf("GC internal allocation (%lu bytes) returning NULL\n",
                        (unsigned long) lb);
         return(0);
@@ -563,17 +564,19 @@ GC_API GC_ATTR_MALLOC void * GC_CALL
     if (!GC_debugging_started) {
         GC_start_debugging_inner();
     }
-    ADD_CALL_CHAIN(result, GC_RETURN_ADDR);
-    return (GC_store_debug_info_inner(result, (word)lb, "INTERNAL", 0));
+    result = GC_store_debug_info_inner(base, (word)lb, "INTERNAL", 0);
+    ADD_CALL_CHAIN(base, GC_RETURN_ADDR);
+    return result;
   }
 
   GC_INNER void * GC_debug_generic_malloc_inner_ignore_off_page(size_t lb,
                                                                 int k)
   {
-    void * result = GC_generic_malloc_inner_ignore_off_page(
+    void *base = GC_generic_malloc_inner_ignore_off_page(
                                 SIZET_SAT_ADD(lb, DEBUG_BYTES), k);
+    void *result;
 
-    if (result == 0) {
+    if (NULL == base) {
         GC_err_printf("GC internal allocation (%lu bytes) returning NULL\n",
                        (unsigned long) lb);
         return(0);
@@ -581,8 +584,9 @@ GC_API GC_ATTR_MALLOC void * GC_CALL
     if (!GC_debugging_started) {
         GC_start_debugging_inner();
     }
-    ADD_CALL_CHAIN(result, GC_RETURN_ADDR);
-    return (GC_store_debug_info_inner(result, (word)lb, "INTERNAL", 0));
+    result = GC_store_debug_info_inner(base, (word)lb, "INTERNAL", 0);
+    ADD_CALL_CHAIN(base, GC_RETURN_ADDR);
+    return result;
   }
 #endif /* DBG_HDRS_ALL */
 
