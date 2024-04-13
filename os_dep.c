a671112220395fc7db9de8ca008233a6a45ad2d4
Ivan Maidanski <ivmai@mail.ru>
2024-04-13 09:18:39 +0300

    Fix potential buffer overrun during read in GC_text_mapping
    * os_dep.c [REDIRECT_MALLOC && GC_LINUX_THREADS] (GC_text_mapping):
    Check p is not less than map_path before p dereference (instead of the
    opposite order).

--- os_dep.c
+++ os_dep.c
@@ -379,7 +379,7 @@ GC_INNER char * GC_get_maps(void)
           char *p = map_path;
           /* Set p to point just past last slash, if any. */
             while (*p != '\0' && *p != '\n' && *p != ' ' && *p != '\t') ++p;
-            while (*p != '/' && (word)p >= (word)map_path) --p;
+            while ((word)p >= (word)map_path && *p != '/') --p;
             ++p;
           if (strncmp(nm, p, nm_len) == 0) {
             *startp = my_start;
