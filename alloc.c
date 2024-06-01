6bfb2001948f6c0cb4ee87832990e66b8dbb4886
Ivan Maidanski <ivmai@mail.ru>
2024-06-01 16:37:19 +0300

    Fix potential address overflow in GC_add_to_heap (a cherry-pick of commit 375fb68d1 from 'master')
    If there is address overflow when adding bytes to p in GC_add_to_heap,
    we might need to subtract HBLKSIZE from bytes more than once.
    
    * alloc.c (GC_add_to_heap): Replace if(endp<=p) to while(endp<=p).

--- alloc.c
+++ alloc.c
@@ -1054,7 +1054,7 @@ GC_INNER void GC_add_to_heap(struct hblk *p, size_t bytes)
         if (0 == bytes) return;
     }
     endp = (word)p + bytes;
-    if (endp <= (word)p) {
+    while (endp <= (word)p) {
         /* Address wrapped. */
         bytes -= HBLKSIZE;
         if (0 == bytes) return;
