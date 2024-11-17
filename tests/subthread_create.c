545e1fcf7c45dda341c747c12ad30861e00b5a56
Ivan Maidanski <ivmai@mail.ru>
2024-11-17 15:30:04 +0300

    Fix 'cannot implement AO_compare_and_swap_full' error by wcc in tests (a cherry-pick of commit 2bf99eaa2 from 'master')
    * tests/test_atomic_ops.c [PARALLEL_MARK && !__GNUC__
    && !AO_ASSUME_WINDOWS98] (AO_ASSUME_WINDOWS98): Define (before include
    `gc_atomic_ops.h`).
    * tests/subthread_create.c [PARALLEL_MARK && !__GNUC__
    && !AO_ASSUME_WINDOWS98] (AO_ASSUME_WINDOWS98): Likewise.

--- tests/subthread_create.c
+++ tests/subthread_create.c
@@ -9,6 +9,9 @@
 
 # ifdef PARALLEL_MARK
 #   define AO_REQUIRE_CAS
+#   if !defined(__GNUC__) && !defined(AO_ASSUME_WINDOWS98)
+#     define AO_ASSUME_WINDOWS98
+#   endif
 # endif
 # include "private/gc_atomic_ops.h"
 #endif /* GC_THREADS */
