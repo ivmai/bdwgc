510ad8bef58276a5d071674b7cb0dd7abd36f933
Ivan Maidanski <ivmai@mail.ru>
2025-02-07 07:10:48 +0300

    Fix usage of new structured procfs interface on Solaris (a cherry-pick of commit d2fa5f1ef from 'master')
    Issue #698 (bdwgc).
    
    * os_dep.c [PROC_VDB]: Update comment.
    * os_dep.c [PROC_VDB && !GC_NO_SYS_FAULT_H]: Do not include
    `sys/fault.h`; include `procfs.h` instead of `sys/procfs.h`; add
    comment.
    * os_dep.c [PROC_VDB] (GC_proc_read_dirty): Replace `sizeof(long)-1` to
    exactly 7 in alignment of `bufp` value; add comment.

--- os_dep.c
+++ os_dep.c
@@ -3501,7 +3501,7 @@ STATIC void GC_protect_heap(void)
 #endif /* MPROTECT_VDB */
 
 #ifdef PROC_VDB
-/* This implementation assumes a Solaris 2.X like /proc                 */
+/* This implementation assumes the Solaris new structured /proc         */
 /* pseudo-file-system from which we can read page modified bits.  This  */
 /* facility is far from optimal (e.g. we would like to get the info for */
 /* only some of the address space), but it avoids intercepting system   */
@@ -3530,8 +3530,8 @@ STATIC void GC_protect_heap(void)
       int dummy2[2];
     };
 # else
-#   include <sys/fault.h>
-#   include <sys/procfs.h>
+    /* Use the new structured /proc definitions. */
+#   include <procfs.h>
 # endif
 
 # ifndef THREADS
@@ -3682,8 +3682,10 @@ GC_INLINE void GC_proc_read_dirty(GC_bool output_unneeded)
                 }
             }
         }
-        bufp = (char *)(((word)bufp + (sizeof(long)-1))
-                        & ~(word)(sizeof(long)-1));
+        /* According to the new structured "pagedata" file format,  */
+        /* an 8-byte alignment is enforced (preceding the next      */
+        /* struct prasmap) regardless of the pointer size.          */
+        bufp = (char *)(((word)bufp + 7) & ~(word)7);
     }
 #   ifdef DEBUG_DIRTY_BITS
       GC_log_printf("Proc VDB read done\n");
