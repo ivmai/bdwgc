fbf46d9c68cd5f628b94dfd532caa0c012961e2e
Tobiasz Laskowski <tobil4sk@outlook.com>
2025-02-15 15:20:09 +0000

    Fix GC_suspend abort when the thread is deleted by DllMain (a cherry-pick of commit 5658d91c8 from 'master')
    PR #705 (bdwgc).
    
    A thread may be deleted by a `DllMain` call during execution of
    `GC_suspend`.
    
    This patch sets the handle to be NULL when deleting the thread to avoid
    potential use-after-free, and skips the `SuspendThread()` attempt if
    the thread handle has been closed.
    
    * win32_threads.c [!MSWINCE] (GC_delete_gc_thread_no_free): Move
    `CloseHandle()` call down to be after `GC_ASSERT(I_HOLD_LOCK())`
    unless `GC_win32_dll_threads` is set.
    * win32_threads.c [!GC_NO_THREADS_DISCOVERY]
    (GC_delete_gc_thread_no_free): Define `handle` local variable; set
    `t->handle` to NULL before `CloseHandle()` (using the atomic store
    with a release barrier); cast `t` to `GC_thread` in `CloseHandle()`
    call.
    * win32_threads.c (GC_delete_thread): Mention `DLL_PROCESS_DETACH`
    case in comment.
    * win32_threads.c [!GC_NO_THREADS_DISCOVERY] (GC_suspend): Return
    immediately if `t->handle` (fetched atomically with an acquire barrier)
    is 0; return instead of `ABORT()` if `SuspendThread()` has failed
    but `t->handle` after it is 0.
    * win32_threads.c [!MSWINCE && RETRY_GET_THREAD_CONTEXT
    && !GC_NO_THREADS_DISCOVERY] (GC_suspend): Return immediately after
    `Sleep(0)` call if `t->handle` is 0.
    
    Co-authored-by: Ivan Maidanski <ivmai@mail.ru>

--- win32_threads.c
+++ win32_threads.c
@@ -707,11 +707,12 @@ GC_API void GC_CALL GC_register_altstack(void *stack GC_ATTR_UNUSED,
 /* thread being deleted.                                        */
 STATIC void GC_delete_gc_thread_no_free(GC_vthread t)
 {
-# ifndef MSWINCE
-    CloseHandle(t->handle);
-# endif
 # ifndef GC_NO_THREADS_DISCOVERY
     if (GC_win32_dll_threads) {
+      HANDLE handle = t -> handle;
+
+      AO_store_release((volatile AO_t *)&t->handle, 0);
+      CloseHandle(handle);
       /* This is intended to be lock-free.                              */
       /* It is either called synchronously from the thread being        */
       /* deleted, or by the joining thread.                             */
@@ -735,6 +736,9 @@ STATIC void GC_delete_gc_thread_no_free(GC_vthread t)
     GC_thread prev = NULL;
 
     GC_ASSERT(I_HOLD_LOCK());
+#   ifndef MSWINCE
+      CloseHandle(((GC_thread)t) -> handle);
+#   endif
     while (p != (GC_thread)t) {
       prev = p;
       p = p -> tm.next;
@@ -753,8 +757,9 @@ STATIC void GC_delete_gc_thread_no_free(GC_vthread t)
 /* (The code intentionally traps if it wasn't.)  Assumes we     */
 /* hold the allocation lock unless GC_win32_dll_threads is set. */
 /* If GC_win32_dll_threads is set then it should be called from */
-/* the thread being deleted.  It is also safe to delete the     */
-/* main thread (unless GC_win32_dll_threads).                   */
+/* the thread being deleted (except for DLL_PROCESS_DETACH      */
+/* case).  It is also safe to delete the main thread (unless    */
+/* GC_win32_dll_threads).                                       */
 STATIC void GC_delete_thread(DWORD id)
 {
   if (GC_win32_dll_threads) {
@@ -1313,6 +1318,10 @@ STATIC void GC_suspend(GC_thread t)
 #   endif
 # endif
 
+# ifndef GC_NO_THREADS_DISCOVERY
+    if (0 == AO_load_acquire((volatile AO_t *)&t->handle))
+      return;
+# endif
 # ifdef DEBUG_THREADS
     GC_log_printf("Suspending 0x%x\n", (int)t->id);
 # endif
@@ -1361,10 +1370,22 @@ STATIC void GC_suspend(GC_thread t)
         /* Resume the thread, try to suspend it in a better location.   */
         if (ResumeThread(t->handle) == (DWORD)-1)
           ABORT("ResumeThread failed in suspend loop");
+      } else {
+#      ifndef GC_NO_THREADS_DISCOVERY
+         if (0 == AO_load_acquire((volatile AO_t *)&t->handle)) {
+           /* The thread handle is closed asynchronously by GC_DllMain. */
+           GC_release_dirty_lock();
+           return;
+         }
+#      endif
       }
       if (retry_cnt > 1) {
         GC_release_dirty_lock();
         Sleep(0); /* yield */
+#       ifndef GC_NO_THREADS_DISCOVERY
+          if (0 == AO_load_acquire((volatile AO_t *)&t->handle))
+            return;
+#       endif
         GC_acquire_dirty_lock();
       }
       if (++retry_cnt >= MAX_SUSPEND_THREAD_RETRIES)
@@ -1382,8 +1403,15 @@ STATIC void GC_suspend(GC_thread t)
 #     endif
       return;
     }
-    if (SuspendThread(t -> handle) == (DWORD)-1)
+    if (SuspendThread(t -> handle) == (DWORD)-1) {
+#     ifndef GC_NO_THREADS_DISCOVERY
+        if (0 == AO_load_acquire((volatile AO_t *)&t->handle)) {
+          GC_release_dirty_lock();
+          return;
+        }
+#     endif
       ABORT("SuspendThread failed");
+    }
 # endif
   t -> suspended = (unsigned char)TRUE;
   GC_release_dirty_lock();
