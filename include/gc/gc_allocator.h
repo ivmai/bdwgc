e35923384edfecc33d8e157e0f9bbde02b0f3d85
Ivan Maidanski <ivmai@mail.ru>
2025-07-29 07:14:53 +0300

    Freshen MIT licensing terms of gc_allocator.h
    Now all MIT-licensed source files are covered with same (new) version
    of MIT license.
    
    * include/gc/gc_allocator.h: Change licensing terms from MIT old-style
    to new MIT (as specified in other `.h` files).

--- include/gc/gc_allocator.h
+++ include/gc/gc_allocator.h
@@ -1,25 +1,15 @@
 /*
- * Copyright (c) 1996-1997
- * Silicon Graphics Computer Systems, Inc.
+ * Copyright (c) 1996-1997 Silicon Graphics Computer Systems, Inc.
+ * Copyright (c) 2002 Hewlett-Packard Company
  *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Silicon Graphics makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
+ * THIS MATERIAL IS PROVIDED AS IS, WITH ABSOLUTELY NO WARRANTY EXPRESSED
+ * OR IMPLIED.  ANY USE IS AT YOUR OWN RISK.
  *
- * Copyright (c) 2002
- * Hewlett-Packard Company
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Hewlett-Packard Company makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
+ * Permission is hereby granted to use or copy this program
+ * for any purpose, provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
  */
 
 /*
